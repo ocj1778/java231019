cceffa8863842a5cc418d4d6bf321eda181f2ae6
itwill <itwill@DESKTOP-AFNN3T8>
2023-10-20 12:48:45 +0900

    2Day

---
+++ java1/src/basic/HelloWorld.java
@@ -0,0 +1,9 @@
+package basic;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.print("Hello, World!!!");
+		System.out.println("안녕하세요.");
+		System.out.println("반갑습니다.");
+	}
+}
