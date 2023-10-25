2e1916530c3d59684090848e2674416376da1526
itwill <itwill@DESKTOP-AFNN3T8>
2023-10-25 14:52:47 +0900

    5Day

--- java1/src/basic/MultiForApp.java
+++ java1/src/basic/MultiForApp.java
@@ -86,27 +86,22 @@ public class MultiForApp {
 			System.out.println();
 		}
 		System.out.println("===============================================================");
-		
-		
-		
+		// 1 2 3 4 5 >> j
+		//☆☆☆☆★  i=1
+		//☆☆☆★★  i=2
+		//☆☆★★★  i=3
+		//☆★★★★  i=4
+		//★★★★★  i=5
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5;j++) {
+				if(i+j>=6) {
+					System.out.print("★");
+				} else {
+					System.out.print("☆");
+				}
+			}
+			System.out.println();
+		}
+		System.out.println("===============================================================");
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
