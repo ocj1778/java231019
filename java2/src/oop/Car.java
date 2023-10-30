14a8ccef1ab66639ea5fd019553d290a9eef280d
itwill <itwill@DESKTOP-AFNN3T8>
2023-10-30 13:11:31 +0900

    8Day

--- java2/src/oop/Car.java
+++ java2/src/oop/Car.java
@@ -22,6 +22,65 @@ package oop;
 
 
 //자동차를 모델링하여 작성된 클래스
+// => 속성 : 모델명, 엔진상태, 현재속도 - 필드
+// => 행위 : 시동 온(On), 시동 오프(Off), 속도 증가, 속도 감소, 이동 중지 - 메소드
 public class Car {
-
+	//필드(Field) : 표현대상의 속성값을 저장하기 위한 변수
+	// => 클래스 내부의 작성된 모든 메소드에서 필드 사용 가능
+	String modelName;//모델명
+	boolean engineStatus;//엔진상태 - false : EngineOff, true : EngineOn
+	int currentSpeed;//현재속도
+	
+	//생성자(Constructor) : 표현대상을 객체를 생성하기 위한 특별한 메소드
+	// => 클래스 내부에 생성자를 작성하지 않으면 매개변수가 없는 기본 생성자(Default Constructor)를
+	//자동으로 생성하여 제공
+	
+	//메소드 : 표현대상의 행위(기능)을 제공하기 위한 함수
+	// => 필드를 사용하여 메소드 내부에 작성된 명령으로 필요한 기능 구현
+	void startEngine() {//시동 온(On)
+		engineStatus=true;
+		System.out.println(modelName+"의 자동차 시동을 켰습니다.");
+	}
+	
+	void stopEngine() {//시동 오프(Off)
+		engineStatus=false;
+		System.out.println(modelName+"의 자동차 시동을 껐습니다.");
+	}
+	
+	void speedUp(int speed) {//속도 증가
+		currentSpeed+=speed;
+		System.out.println(modelName+"의 자동차 속도가 "+speed
+				+"Km/h 증가 되었습니다. 현재 속도는 "+currentSpeed+"Km/h 입니다.");
+	}
+	
+	void speedDwon(int speed) {//속도 감소
+		currentSpeed-=speed;
+		System.out.println(modelName+"의 자동차 속도가 "+speed
+				+"Km/h 감소 되었습니다. 현재 속도는 "+currentSpeed+"Km/h 입니다.");
+	}
+	
+	void speedZero() {//이동 중지
+		currentSpeed=0;
+		System.out.println(modelName+"의 자동차가 멈췄습니다.");
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
