cf30413887dfffcf749cf54398675bed2f28506d
itwill <itwill@DESKTOP-AFNN3T8>
2023-11-02 18:08:49 +0900

    11Day

--- java2/src/inheritance/Car.java
+++ java2/src/inheritance/Car.java
@@ -1,7 +1,11 @@
 package inheritance;
 
 //자동차 정보(모델명, 소유자명)를 저장하기 위한 클래스
-public class Car {
+// => 클래스를 작성시 상속받은 부모클래스가 없는 경우 무조건 Object 클래스를 자동으로 상속받아 사용
+// => 모든 Java 클래스는 Object 클래스를 상속받기 때문에 Object 클래스의 메소드 사용 가능
+//Object 클래스 : 모든 Java 클래스의 부모클래스 - 최선조 클래스
+// => Object 클래스로 생성된 참조변수에는 모든 클래스로 생성된 객체 저장 가능
+public class Car /* extends Object */ {
 	private String modelName;
 	private String userName;
 	
@@ -29,5 +33,13 @@ public class Car {
 
 	public void setUserName(String userName) {
 		this.userName = userName;
+	}
+	
+	//Object 클래스(부모클래스)의 toString() 메소드를 오버라이드 선언
+	// => Object 클래스의 toString() 메소드는 숨겨지고 자식클래스의 toString() 메소드 사용
+	// => VO 클래스의 모든 필드값을 문자열로 변환하여 반환하는 기능 제공 - 필드값 확인
+	@Override
+	public String toString() {
+		return "modelName : "+modelName+", userName : "+userName;
 	}
 }
--- java2/src/inheritance/CarApp.java
+++ java2/src/inheritance/CarApp.java
@@ -1,5 +1,32 @@
 package inheritance;
 
 public class CarApp {
-
+	public static void main(String[] args) {
+		Car car=new Car("싼타페", "홍길동");
+		
+		System.out.println("모델명 = "+car.getModelName());
+		System.out.println("소유자 = "+car.getUserName());
+		System.out.println("==============================================================");
+		/*
+		//Object.toString() : 참조변수에 저장된 객체를 참조하여 "클래스명@메모리주소" 형식의
+		//문자열로 변환하여 반환하는 메소드
+		System.out.println("car.toString() = "+car.toString());
+		//참조변수를 출력 처리할 경우 자동으로 Object 객체를 참조하여 toString() 메소드를 자동 호출
+		// => 참조변수를 출력할 경우 toString() 메소드 호출은 생략 가능
+		System.out.println("car = "+car);
+		*/
+		
+		//묵시적 객체 형변환에 의해 Object 클래스의 toString() 메소드가 아닌 Car 클래스에
+		//작성된 toString() 메소드가 자동 호출되어 Car 객체의 필드값 출력
+		System.out.println("car = "+car);
+		System.out.println("==============================================================");
+		//문자열이 저장된 String 객체를 생성하여 메모리 주소를 String 클래스의 참조변수에 저장
+		String name="홍길동";
+		
+		//String.toString() : String 객체에 저장된 문자열을 반환하는 메소드
+		//System.out.println("이름 = "+name.toString());
+		//참조변수를 출력할 경우 String 클래스에 오버라이드 선언된 toString() 메소드 자동 호출
+		System.out.println("이름 = "+name);
+		System.out.println("==============================================================");
+	}
 }
