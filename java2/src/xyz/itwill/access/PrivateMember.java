6b82ef7420ec54eda08929cd5cd00485a8cd57eb
itwill <itwill@DESKTOP-AFNN3T8>
2023-11-07 10:35:23 +0900

    13Day

--- java2/src/xyz/itwill/access/PrivateMember.java
+++ java2/src/xyz/itwill/access/PrivateMember.java
@@ -6,7 +6,7 @@ package xyz.itwill.access;
 
 //private : 필드, 생성자, 메소드 작성시 사용 가능한 접근 제한자
 // => 필드, 생성자, 메소드를 클래스 내부에서만 사용 가능하도록 설정하는 접근 제한자 - 은닉화
-// => 다른 클래스에서  필드, 생성자, 메소드를 사용할 경우 에러 발생
+// => 다른 클래스에서 필드, 생성자, 메소드를 사용할 경우 에러 발생
 public class PrivateMember {
 	private int number;
 	
