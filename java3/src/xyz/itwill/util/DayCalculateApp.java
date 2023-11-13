4e6807011c732c163c7e4b6338577ffbf1b67220
itwill <itwill@DESKTOP-AFNN3T8>
2023-11-13 18:02:44 +0900

    18Day

---
+++ java3/src/xyz/itwill/util/DayCalculateApp.java
@@ -0,0 +1,11 @@
+package xyz.itwill.util;
+
+//키보드로 생년월일을 입력받아 오늘까지 살아온 날짜(일)을 계산하여 출력하는 프로그램 작성
+//ex) 생년월일 입력[ex. 2000-01-01] >> 2023-11-13
+//    [결과]태어난지 <1일>이 지났습니다. 
+// => 형식에 맞지 않는 생년월일을 입력한 경우 에러 메세지 출력 후 프로그램 종료
+public class DayCalculateApp {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ java3/src/xyz/itwill/util/WantCalendarApp.java
@@ -0,0 +1,8 @@
+package xyz.itwill.util;
+
+//키보드로 [년]과 [월]을 입력받아 해당 년월에 대한 달력을 출력하는 프로그램 작성
+public class WantCalendarApp {
+	public static void main(String[] args) {
+		
+	}
+}
