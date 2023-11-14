5f3ffe06485e306ea0cfae4bd552cbc17cbb746d
itwill <itwill@DESKTOP-AFNN3T8>
2023-11-14 17:16:19 +0900

    19Day

--- java3/src/xyz/itwill/util/Student.java
+++ java3/src/xyz/itwill/util/Student.java
@@ -1,7 +1,12 @@
 package xyz.itwill.util;
 
+//Comparable 인터페이스 : 값을 비교하여 결과를 반환하는 compareTo() 추상메소드가 선언
+// => compareTo() 메소드를 오버라이드 선언하여 자식클래스의 객체 필드값을 매개변수로 전달받은
+//객체의 필드값과 비교하여 양수, 음수, 0 중 하나를 반환
+// => 객체를 비교하기 위해 사용하는 인터페이스 - 정렬 처리할 때 반드시 필요
+
 //학생정보(학번, 이름)를 저장하기 위한 클래스
-public class Student {
+public class Student implements Comparable<Student> {
 	private int num;
 	private String name;
 	
@@ -33,6 +38,27 @@ public class Student {
 	
 	@Override
 	public String toString() {
-		return "학번 = "+num+", 이름 = "+name;
+		return "{학번 = "+num+", 이름 = "+name+"}";
+	}
+
+	@Override
+	public int compareTo(Student o) {
+		//return this.num-o.num;//학번을 비교하여 결과값(양수,음수,0) 반환 - 오름차순 정렬
+		//return o.num-this.num;//학번을 비교하여 결과값(양수,음수,0) 반환 - 내림차순 정렬
+		//문자열은 String 클래스의 compareTo() 메소드를 호출하여 비교하여 반환값으로 처리
+		//return this.name.compareTo(o.name);//이름을 비교하여 결과값(양수,음수,0) 반환 - 오름차순 정렬
+		return o.name.compareTo(this.name);//이름을 비교하여 결과값(양수,음수,0) 반환 - 내림차순 정렬
+		
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
---
+++ java3/src/xyz/itwill/util/StudentApp.java
@@ -0,0 +1,55 @@
+package xyz.itwill.util;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class StudentApp {
+	public static void main(String[] args) {
+		List<Student> studentListOne=new ArrayList<Student>();
+		studentListOne.add(new Student(1000, "홍길동"));
+		studentListOne.add(new Student(2000, "임꺽정"));
+		studentListOne.add(new Student(3000, "전우치"));
+		studentListOne.add(new Student(4000, "일지매"));
+		studentListOne.add(new Student(5000, "장길산"));
+		
+		System.out.print("정렬 전 >> ");
+		System.out.println(studentListOne);
+		
+		//Collections 클래스의 sort() 메소드를 호출하면 List 객체에 저장된 모든 요소값(객체 필드값)을 
+		//비교하여 정렬하기 위해 Student 클래스의 compareTo() 메소드를 자동 호출하여 비교 처리
+		Collections.sort(studentListOne);
+		
+		System.out.print("정렬 후 >> ");
+		System.out.println(studentListOne);
+		System.out.println("==============================================================");
+		List<Student> studentListTwo=new ArrayList<Student>();
+		studentListTwo.add(new Student(6000, "유재석"));
+		studentListTwo.add(new Student(7000, "강호동"));
+		studentListTwo.add(new Student(8000, "신동엽"));
+		studentListTwo.add(new Student(9000, "김용만"));
+		
+		Map<Integer, List<Student>> studentListMap=new HashMap<Integer, List<Student>>();
+		studentListMap.put(1, studentListOne);
+		studentListMap.put(2, studentListTwo);
+		
+		for(Integer ban : studentListMap.keySet()) {
+			System.out.println(ban+"반의 학생정보 >> ");
+			
+			List<Student> studentList=studentListMap.get(ban);
+			for(Student student : studentList) {
+				System.out.println(student);//Student 클래스의 toString() 메소드 자동 호출
+			}
+			
+			System.out.println();
+		}
+		System.out.println("==============================================================");
+	}
+
+
+
+
+
+}
