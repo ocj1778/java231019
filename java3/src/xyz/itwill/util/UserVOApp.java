aa09433f980d0130463480aa89ca4c47b02e1864
itwill <itwill@DESKTOP-AFNN3T8>
2023-11-14 13:26:46 +0900

    19Day

--- java3/src/xyz/itwill/util/UserVOApp.java
+++ java3/src/xyz/itwill/util/UserVOApp.java
@@ -16,8 +16,8 @@ public class UserVOApp {
 		//UserVO 클래스에 equals() 메소드와 hashCode() 메소드를 오버라이드 하지 않은 경우
 		//객체의 필드값은 같아도 객체가 다르므로 Set 객체의 요소값으로 저장 가능
 		// => UserVO 클래스에 equals() 메소드와 hashCode() 메소드를 오버라이드 선언하면
-		//Set 객체의 메소드 호출시 equals() 메소드와 hashCode() 메소드가 자동 호출되어
-		//필드값과 메모리 주소(HashCode)가 같은 객체를 Set 객체의 요소값으로 저장 불가
+		//Set 객체의 메소드 호출시 equals() 메소드와 hashCode() 메소드가 자동 호출되어 필드값과
+		//메모리 주소(HashCode)가 같은 동일한 객체가 Set 객체의 요소값으로 저장하지 않도록 처리
 		set.add(new UserVO("abc123", "홍길동", "abc@itwill.xyz"));
 
 		System.out.println("set = "+set);
