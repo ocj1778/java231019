e3ae87738a9d8a4fa370afe7aace0092c016b063
itwill <itwill@DESKTOP-AFNN3T8>
2023-11-14 12:06:12 +0900

    19Day

--- java3/src/xyz/itwill/util/DayCalculateApp.java
+++ java3/src/xyz/itwill/util/DayCalculateApp.java
@@ -1,11 +1,36 @@
 package xyz.itwill.util;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 //키보드로 생년월일을 입력받아 오늘까지 살아온 날짜(일)을 계산하여 출력하는 프로그램 작성
 //ex) 생년월일 입력[ex. 2000-01-01] >> 2023-11-13
 //    [결과]태어난지 <1일>이 지났습니다. 
 // => 형식에 맞지 않는 생년월일을 입력한 경우 에러 메세지 출력 후 프로그램 종료
 public class DayCalculateApp {
 	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+
+		SimpleDateFormat dateFormat=new SimpleDateFormat("yyyy-MM-dd");
+		Date birthday=null; 
 		
+		System.out.print("생년월일 입력[ex. 2000-01-01] >> ");
+		try {
+			//키보드로 입력받은 문자열(생년월일)을 Date 객체로 변환하여 저장
+			// => 키보드로 입력받은 문자열이 SimpleDateFormat 객체에 저장된 패턴정보가 일치하지
+			//않을 경우 ParseException 발생
+			birthday=dateFormat.parse(scanner.nextLine());
+		} catch (ParseException e) {
+			System.out.println("[에러]생년월일을 형식에 맞게 입력해 주세요.");
+			System.exit(0);
+		} finally {
+			scanner.close();
+		}
+		
+		//살아온 날짜를 계산하여 출력
+		long day=(System.currentTimeMillis()-birthday.getTime())/(1000*60*60*24);		
+		System.out.println("[결과]태어난지 <"+day+">일 지났습니다.");
 	}
 }
--- java3/src/xyz/itwill/util/WantCalendarApp.java
+++ java3/src/xyz/itwill/util/WantCalendarApp.java
@@ -1,8 +1,54 @@
 package xyz.itwill.util;
 
+import java.util.Calendar;
+import java.util.Scanner;
+
 //키보드로 [년]과 [월]을 입력받아 해당 년월에 대한 달력을 출력하는 프로그램 작성
 public class WantCalendarApp {
 	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
 		
+		System.out.print("년 입력 >> ");
+		int year=scanner.nextInt();
+		
+		System.out.print("월 입력 >> ");
+		int month=scanner.nextInt();
+		
+		scanner.close();
+
+		//플렛폼의 현재 날짜와 시간 관련 정보가 저장된 Calendar 객체를 반환받아 저장
+		Calendar calendar=Calendar.getInstance();
+		
+		//Calendar 객체에 저장된 날짜와 시간 관련 정보 중 년월일을 키보드로 입력받은 [년]과
+		//[월]의 [1일]로 변경 처리
+		// => Calendar 객체는 월을 0~11 범위의 정수값으로 표현하므로 키보드 입력값을 [1] 감소하여 변경
+		calendar.set(year, month-1, 1);
+		
+		int week=calendar.get(Calendar.DAY_OF_WEEK);
+		
+		System.out.println("          "+year+"년 "+month+"월");
+		System.out.println("============================");
+		System.out.println("  일  월  화  수  목  금  토");
+		System.out.println("============================");
+		
+		for(int i=1;i<week;i++) {
+			System.out.print("    ");
+		}
+		
+		for(int i=1;i<=calendar.getActualMaximum(Calendar.DATE);i++) {
+			if(i <= 9) {
+				System.out.print("   "+i);
+			} else {
+				System.out.print("  "+i);
+			}
+			
+			week++;
+			
+			if(week % 7 == 1) {
+				System.out.println();
+			}
+		}
+		System.out.println();
+		System.out.println("============================");
 	}
 }
