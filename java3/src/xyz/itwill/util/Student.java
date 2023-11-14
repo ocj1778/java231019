4b14f7a013f7a766cafd69d4bb50b2302747d33c
itwill <itwill@DESKTOP-AFNN3T8>
2023-11-14 16:41:12 +0900

    19Day

---
+++ java3/src/xyz/itwill/util/Student.java
@@ -0,0 +1,38 @@
+package xyz.itwill.util;
+
+//학생정보(학번, 이름)를 저장하기 위한 클래스
+public class Student {
+	private int num;
+	private String name;
+	
+	public Student() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student(int num, String name) {
+		super();
+		this.num = num;
+		this.name = name;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "학번 = "+num+", 이름 = "+name;
+	}
+}
