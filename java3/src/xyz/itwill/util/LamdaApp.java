3beb45edee9a6248c6d153531e645c230377313d
itwill <itwill@DESKTOP-AFNN3T8>
2023-11-15 15:26:22 +0900

    20Day

--- java3/src/xyz/itwill/util/LamdaApp.java
+++ java3/src/xyz/itwill/util/LamdaApp.java
@@ -1,6 +1,7 @@
 package xyz.itwill.util;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 //java.util.function 패키지 : 함수형 인터페이스가 선언된 패키지
@@ -10,8 +11,8 @@ public class LamdaApp {
 	public static void main(String[] args) {
 		int number=100;
 		
-		//Consumer 함수형 인터페이스 : 제네릭으로 전달된 Java 자료형의 객체(값)을 하나만 
-		//매개변수로 제공받아 아무것도 반환하지 않는 accept() 추상메소드가 선언된 인터페이스
+		//Consumer<T> 함수형 인터페이스 : 제네릭으로 전달된 Java 자료형의 객체(값)을 매개변수로 
+		//제공받아 아무것도 반환하지 않는 accept() 추상메소드가 선언된 함수형 인터페이스
 		
 		/*
 		//익명클래스를 이용하여 객체를 생성해 참조변수에 저장
@@ -24,20 +25,32 @@ public class LamdaApp {
 		*/
 		
 		//람다표현식을 이용하여 객체를 생성해 참조변수에 저장
-		Consumer<Integer> integerConsumer = (i) -> {
+		Consumer<Integer> integerConsumer = i -> {
 			System.out.println(number + i);
 		};
 
 		integerConsumer.accept(200);
 		
-		//Predicate 함수형 인터페이스 : 제네릭으로 전달된 Java 자료형의 객체(값)을 하나만 
-		//매개변수로 제공받아 [boolean]를 반환하는 test() 추상메소드가 선언된 인터페이스
-		Predicate<Integer> isEven = (i) -> i % 2 == 0;
+		//Predicate<T> 함수형 인터페이스 : 제네릭으로 전달된 Java 자료형의 객체(값)을 매개변수로 
+		//제공받아 [boolean]를 반환하는 test() 추상메소드가 선언된 함수형 인터페이스
+		Predicate<Integer> isEven = i -> i % 2 == 0;
 		
 		if(isEven.test(10)) {
 			System.out.println("입력값은 [짝수]입니다.");
 		} else {
 			System.out.println("입력값은 [홀수]입니다.");
 		}
+		
+		//Function<T, R> 함수형 인터페이스 : 첫번째 제네릭으로 전달된 Java 자료형의 객체를
+		//매개변수로 전달받아 처리하여 두번째 제네릭으로 전달된 Java 자료형의 객체로 반환하는
+		//apply() 추상메소드가 선언된 함수형 인터페이스
+		Function<String, Integer> stringLength = str -> str.length();
+
+		System.out.println("문자 갯수 = "+stringLength.apply("Java Programming"));
 	}
 }
+
+
+
+
+
---
+++ java3/src/xyz/itwill/util/PersonApp.java
@@ -0,0 +1,50 @@
+package xyz.itwill.util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class PersonApp {
+	//메소드의 반환형 또는 매개변수를 제네릭을 사용하여 작성한 경우 메소드 반환형에 앞에 사용할 
+	//제네릭을 작성하여 메소드 선언
+	public static <T> void forEach(List<T> list, Consumer<T> c) {
+		//List 객체의 요소값을 차례대로 제공받아 변수에 저장하여 반복 처리
+		for(T t : list) {
+			c.accept(t);
+		}
+		System.out.println();
+	}
+	
+	public static <T> List<T> filter(List<T> list, Predicate<T> predicate) {
+		List<T> result=new ArrayList<T>();
+		
+		for(T t : list) {
+			if(predicate.test(t)) {
+				result.add(t);
+			}
+		}
+		
+		return result;
+	}
+ 	
+	public static void main(String[] args) {
+		forEach(Arrays.asList("홍길동","임꺽정","전우치"), str -> System.out.print(str+" "));
+		//List.of(E... element) : 매개변수로 전달받은 0개 이상의 객체가 저장된 List 객체를
+		//반환하는 정적메소드
+		forEach(List.of(100, 200, 300), num -> System.out.print(num+" "));
+		System.out.println("==============================================================");
+		List<Person> personList=new ArrayList<Person>();
+		personList.add(new Person("홍길동", 29));
+		personList.add(new Person("임꺽정", 35));
+		personList.add(new Person("전우치", 50));
+		
+		System.out.println(personList);
+		System.out.println("==============================================================");
+		List<Person> thirtyList=filter(personList, person -> person.getAge() >= 30);
+		
+		System.out.println(thirtyList);
+		System.out.println("==============================================================");
+	}
+}
