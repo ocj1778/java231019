f7d9366a55702be06e49731c73fb2640ac0903dc
itwill <itwill@DESKTOP-AFNN3T8>
2023-11-16 18:08:32 +0900

    21Day

---
+++ java3/src/xyz/itwill/swing/CalculatorFrameApp.java
@@ -0,0 +1,154 @@
+package xyz.itwill.swing;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+//사칙 연산식을 입력받아 연산 결과를 출력하는 프로그램
+public class CalculatorFrameApp extends JFrame implements ActionListener {
+	private static final long serialVersionUID = 1L;
+
+	//연산식을 입력하기 위한 필드(컴퍼넌트)
+	private JButton b_0, b_1, b_2, b_3, b_4, b_5, b_6, b_7, b_8, b_9, b_equals, b_plus,
+			b_minus, b_multi, b_div, b_C;
+
+	//연산 결과를 출력하기 위한 필드(컴퍼넌트)
+	private JLabel label;
+
+	//연산식을 저장하기 위한 필드
+	private String operation="";
+	
+	public CalculatorFrameApp(String title) {
+		super(title);
+		initButtons();
+		init();
+	}
+
+	private void init() {
+		label = new JLabel("0");
+		label.setFont(new Font("DIALOG", Font.BOLD, 30));
+		label.setHorizontalAlignment(JLabel.RIGHT);
+		label.setBackground(Color.LIGHT_GRAY);
+		label.setForeground(Color.WHITE);
+
+		JPanel p = new JPanel(new GridLayout(4, 4, 5, 5));
+		p.setBackground(Color.BLACK);
+		
+		p.add(b_multi);
+		p.add(b_div);
+		p.add(b_plus);
+		p.add(b_minus);
+		p.add(b_1);
+		p.add(b_2);
+		p.add(b_3);
+		p.add(b_4);
+		p.add(b_5);
+		p.add(b_6);
+		p.add(b_7);
+		p.add(b_8);
+		p.add(b_9);
+		p.add(b_0);
+		p.add(b_equals);
+		p.add(b_C);
+		
+		Container container=getContentPane();
+		container.setLayout(new BorderLayout(10, 10));
+		container.setBackground(Color.BLACK);
+		container.add(label, BorderLayout.NORTH);
+		container.add(p, BorderLayout.CENTER);
+		
+		b_0.addActionListener(this);
+		b_1.addActionListener(this);
+		b_2.addActionListener(this);
+		b_3.addActionListener(this);
+		b_4.addActionListener(this);
+		b_5.addActionListener(this);
+		b_6.addActionListener(this);
+		b_7.addActionListener(this);
+		b_8.addActionListener(this);
+		b_9.addActionListener(this);
+		b_div.addActionListener(this);
+		b_plus.addActionListener(this);
+		b_minus.addActionListener(this);
+		b_multi.addActionListener(this);
+		b_C.addActionListener(this);
+		b_equals.addActionListener(this);
+		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		setResizable(false);
+		setBounds(500, 100, 400, 400);
+		setVisible(true);
+	}
+
+	private void initButtons() {
+		b_0 = new JButton("0");
+		b_1 = new JButton("1");
+		b_2 = new JButton("2");
+		b_3 = new JButton("3");
+		b_4 = new JButton("4");
+		b_5 = new JButton("5");
+		b_6 = new JButton("6");
+		b_7 = new JButton("7");
+		b_8 = new JButton("8");
+		b_9 = new JButton("9");
+		b_equals = new JButton("=");
+		b_plus = new JButton("+");
+		b_minus = new JButton("-");
+		b_multi = new JButton("*");
+		b_div = new JButton("/");
+		b_C = new JButton("C");
+
+		b_0.setFont(new Font("DIALOG", Font.PLAIN, 20));
+		b_1.setFont(new Font("DIALOG", Font.PLAIN, 20));
+		b_2.setFont(new Font("DIALOG", Font.PLAIN, 20));
+		b_3.setFont(new Font("DIALOG", Font.PLAIN, 20));
+		b_4.setFont(new Font("DIALOG", Font.PLAIN, 20));
+		b_5.setFont(new Font("DIALOG", Font.PLAIN, 20));
+		b_6.setFont(new Font("DIALOG", Font.PLAIN, 20));
+		b_7.setFont(new Font("DIALOG", Font.PLAIN, 20));
+		b_8.setFont(new Font("DIALOG", Font.PLAIN, 20));
+		b_9.setFont(new Font("DIALOG", Font.PLAIN, 20));
+		b_div.setFont(new Font("DIALOG", Font.PLAIN, 20));
+		b_plus.setFont(new Font("DIALOG", Font.PLAIN, 20));
+		b_minus.setFont(new Font("DIALOG", Font.PLAIN, 20));
+		b_multi.setFont(new Font("DIALOG", Font.PLAIN, 20));
+		b_C.setFont(new Font("DIALOG", Font.PLAIN, 20));
+		b_equals.setFont(new Font("DIALOG", Font.PLAIN, 20));
+		
+		b_0.setBackground(Color.WHITE);
+		b_1.setBackground(Color.WHITE);
+		b_2.setBackground(Color.WHITE);
+		b_3.setBackground(Color.WHITE);
+		b_4.setBackground(Color.WHITE);
+		b_5.setBackground(Color.WHITE);
+		b_6.setBackground(Color.WHITE);
+		b_7.setBackground(Color.WHITE);
+		b_8.setBackground(Color.WHITE);
+		b_9.setBackground(Color.WHITE);
+		b_div.setBackground(Color.YELLOW);
+		b_plus.setBackground(Color.YELLOW);
+		b_minus.setBackground(Color.YELLOW);
+		b_multi.setBackground(Color.YELLOW);
+		b_C.setBackground(Color.GREEN);
+		b_equals.setBackground(Color.CYAN);
+	}
+
+	public static void main(String[] args) {
+		new CalculatorFrameApp("계산기");
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+}
---
+++ java3/src/xyz/itwill/swing/SwingApp.java
@@ -0,0 +1,92 @@
+package xyz.itwill.swing;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+//GUI 프로그램의 UI 클래스는 java.awt 패키지의 컴퍼넌트를 이용하여 작성하는 것보다 javax.swing 
+//패키지의 컴퍼넌트를 사용하여 작성하는 것을 권장
+// => javax.swing 패키지의 컴퍼넌트는 Java 프로그램에서 제공하는 컴퍼넌트 - 플렛폼에 독립적으로 실행
+
+//javax.swing 패키지의 컴퍼넌트(컨테이너)로 UI 클래스를 작성하는 방법 - AWT 프로그램과의 차이점
+//1.java.awt 패키지의 컴퍼넌트(컨테이너)를 구현한 클래스 이름 앞에 [J]를 붙여 javax.swing
+//패키지의 컴퍼넌트(컨테이너)로 구현
+//2.프레임의 [닫기] 버튼을 누른 경우 동작되는 기능을 기본적으로 제공
+// => JFrame.setDefaultCloseOperation(int operation) 메소드를 호출하여 프레임의 [닫기] 버튼을
+//누른 경우 동작되는 기능을 변경 가능
+//3.프레임을 직접 변경하지 않고 프레임의 Container 객체를 반환받아 변경 처리
+// => 프레임의 배치관리자 변경, 프레임의 배경색 변경, 프레임에 컴퍼넌트 배치 등
+
+//JTextField 컴퍼넌트에서 메세지를 입력한 후 엔터를 누르면 JTextArea 컴퍼넌트에 메세지를
+//추가하여 출력되도록 프로그램 작성
+// => JTextField 컴퍼넌트에서 문자열을 입력 후 엔터를 누르면 ActionEvent 발생
+public class SwingApp extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+	JTextArea textArea;
+	JTextField textField;
+	
+	public SwingApp(String title) {
+		super(title);
+
+		textArea=new JTextArea("[홍길동]님이 입장 하였습니다.\n");
+		textField=new JTextField();
+		
+		//Swing 컴퍼넌트에서는 플렛폼에서 제공하는 모든 글꼴 사용 가능
+		textArea.setFont(new Font("굴림체", Font.BOLD, 20));
+		textField.setFont(new Font("굴림체", Font.BOLD, 20));
+		
+		textArea.setBackground(Color.PINK);
+		
+		//JTextArea 컴퍼넌트에 입력 포커스가 제공되지 않도록 설정
+		// => JTextArea 컴퍼넌트를 출력 전용 컴퍼넌트로 사용
+		textArea.setFocusable(false);
+		
+		//JFrame.getContentPane() : 프레임의 Container 객체를 반환하는 메소드
+		Container container=getContentPane();
+		
+		container.add(textArea, BorderLayout.CENTER);
+		container.add(textField, BorderLayout.SOUTH);
+		
+		//setDefaultCloseOperation(int operation) 메소드의 매개변수에는 WindowConstants 클래스의 상수 전달
+		// => DO_NOTHING_ON_CLOSE : 아무런 동작도 실행되지 않도록 설정하는 상수
+		// => HIDE_ON_CLOSE : 프레임이 보여지지 않도록 설정하는 상수 - 기본값
+		// => DISPOSE_ON_CLOSE : 메모리를 정리한 후 프로그램을 종료되도록 설정하는 상수
+		// => EXIT_ON_CLOSE : 프로그램이 무조건 종료되도록 설정하는 상수
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		textField.addActionListener(new TextComponentEventHandle());
+		
+		setBounds(800, 200, 400, 500);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new SwingApp("Swing");
+	}
+	
+	public class TextComponentEventHandle implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			//TextComponent.getText() : JTextField 컴퍼넌트 또는 JTextArea 컴퍼넌트에 입력된
+			//문자열을 얻어와 반환하는 메소드
+			String text=textField.getText();
+			
+			if(!text.equals("")) {//입력된 문자열이 있는 경우
+				//JTextArea.append(String text) : JTextArea 컴퍼넌트에 문자열을 추가하여 출력하는 메소드
+				textArea.append("[홍길동]"+text+"\n");
+				
+				//TextComponent.setText(String text) : JTextField 컴퍼넌트 또는 JTextArea 컴퍼넌트의 
+				//문자열을 변경하는 메소드
+				textField.setText("");				
+			}
+		}
+	}
+}
