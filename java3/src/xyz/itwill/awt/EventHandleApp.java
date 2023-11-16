73f04da5c5c5cb28d4c36de8ae2dbd309e33b2ea
itwill <itwill@DESKTOP-AFNN3T8>
2023-11-16 15:02:19 +0900

    21Day

--- java3/src/xyz/itwill/awt/EventHandleApp.java
+++ java3/src/xyz/itwill/awt/EventHandleApp.java
@@ -19,11 +19,16 @@ import java.awt.event.ActionListener;
 //2.이벤트 소스에서 발생된 이벤트를 처리하기 위한 클래스 작성
 // => 이벤트 처리 클래스는 발생 이벤트를 처리하기 위해 XXXListener 인터페이스를 상속받아 작성
 // ex) ActionEvent >> ActionListener 인터페이스를 상속받아 이벤트 처리 클래스 작성
-// => Listener 인터페이스에는 이벤트를 처리하기 위한 추상메소드가 선언
+// => Listener 인터페이스에는 이벤트를 처리하기 위한 추상메소드가 1개 이상 선언
 // => 이벤트 처리 클래스에서는 Listener 인터페이스의 추상메소드를 오버라이드 선언하여 이벤트 처리 명령 작성
+//3.이벤트 소스에서 이벤트가 발생된 경우 이벤트 처리 클래스로 생성된 객체를 제공받아 이벤트
+//처리 메소드가 자동 호출되어 이벤트 처리되도록 설정
+// => Component.addXXXListener(Listener l) : 컴퍼넌트에 발생된 이벤트를 처리하기 위한 이벤트 처리 
+//클래스의 객체를 매개변수로 전달받아 이벤트 처리 메소드를 자동 호출되도록 추가(등록)하는 메소드
+// => 이벤트가 발생될 경우 이벤트 처리하는 스레드는 [main] 스레드가 아닌 이벤트 스레드 사용
 
-
-//[EXIT] 버튼을 누른 경우 프로그램을 종료하는 프로그램 작성
+//[EXIT] 버튼을 누른 경우 프로그램을 종료하는 기능의 프로그램 작성
+// => 이벤트 처리 클래스의 메소드에서 UI 클래스의 컴퍼넌트 또는 컨테이너 사용 불가능
 public class EventHandleApp extends Frame {
 	private static final long serialVersionUID = 1L;
 
@@ -35,6 +40,8 @@ public class EventHandleApp extends Frame {
 		exit.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 30));
 		add(exit);
 
+		//Button 컴퍼넌트에서 발생된 ActionEvent를 이벤트 처리 클래스의 객체로 처리되도록 등록
+		// => 이벤트 처리 클래스의 객체는 이벤트 처리 메소드를 자동으로 호출하여 이벤트 처리 명령 실행
 		exit.addActionListener(new EventHandle());
 		
 		setBounds(800, 200, 300, 300);
@@ -56,13 +63,3 @@ class EventHandle implements ActionListener {
 		System.exit(0);
 	}
 }
-
-
-
-
-
-
-
-
-
-
---
+++ java3/src/xyz/itwill/awt/EventInnerHandleApp.java
@@ -0,0 +1,42 @@
+package xyz.itwill.awt;
+
+import java.awt.Button;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+//[EXIT] 버튼을 누른 경우 프로그램을 종료하는 기능의 프로그램 작성
+// => 이벤트 처리 클래스를 UI 클래스의 내부 클래스로 작성하여 사용
+// => 이벤트 처리 클래스의 메소드에서 UI 클래스의 컴퍼넌트 또는 컨테이너 사용 가능
+public class EventInnerHandleApp extends Frame {
+	private static final long serialVersionUID = 1L;
+	
+	public EventInnerHandleApp(String title) {
+		super(title);
+		
+		setLayout(new FlowLayout());
+		Button exit=new Button("EXIT");
+		exit.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 30));
+		add(exit);
+
+		exit.addActionListener(new EventHandle());
+		
+		setBounds(800, 200, 300, 300);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new EventInnerHandleApp("이벤트 처리");
+	}
+	
+	//내부클래스(이벤트 처리 클래스)는 외부클래스(UI 클래스)의 필드와 메소드를 접근제한자에
+	//상관없이 사용 가능
+	public class EventHandle implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			System.exit(0);
+		}
+	}
+}
