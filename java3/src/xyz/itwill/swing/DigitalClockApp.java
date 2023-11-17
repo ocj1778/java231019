537bf422a8ee557fbec68c7420195cb81c48aef4
itwill <itwill@DESKTOP-AFNN3T8>
2023-11-17 14:52:05 +0900

    21Day

--- java3/src/xyz/itwill/swing/DigitalClockApp.java
+++ java3/src/xyz/itwill/swing/DigitalClockApp.java
@@ -5,8 +5,10 @@ import java.awt.Font;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 //플렛폼의 현재 날짜와 시간을 1초마다 제공받아 출력하는 GUI 프로그램 작성
 // => 새로운 스레드를 생성하여 플렛폼의 현재 날짜와 시간을 1초마다 제공받아 컴퍼넌트 변경 처리
@@ -14,6 +16,7 @@ public class DigitalClockApp extends JFrame {
 	private static final long serialVersionUID = 1L;
 
 	JLabel clockLable;
+	JButton startButton, stopButton;
 	
 	public DigitalClockApp(String title) {
 		super(title);
@@ -28,7 +31,18 @@ public class DigitalClockApp extends JFrame {
 		//JLabel clockLable=new JLabel(printDate, JLabel.CENTER);
 		clockLable=new JLabel("", JLabel.CENTER);
 		clockLable.setFont(new Font("굴림체", Font.BOLD, 30));
-	
+		
+		startButton=new JButton("다시 실행");
+		stopButton=new JButton("일시 중지");
+		startButton.setFont(new Font("굴림체", Font.BOLD, 20));
+		stopButton.setFont(new Font("굴림체", Font.BOLD, 20));
+		
+		startButton.setEnabled(false);
+		
+		JPanel panel=new JPanel();
+		panel.add(startButton);
+		panel.add(stopButton);
+		
 		//스레드 클래스로 객체를 생성하여 start() 메소드를 호출해 새로운 스레드를 만들어
 		//run() 메소드의 명령 실행
 		//new ClockThread().start();
@@ -61,9 +75,7 @@ public class DigitalClockApp extends JFrame {
 			SimpleDateFormat dateFormat=new SimpleDateFormat("yyyy년 MM월 dd일 HH시 mm분 ss초");
 
 			while(true) {
-				Date now=new Date();
-				String printDate=dateFormat.format(now);
-				clockLable.setText(printDate);//JLabel 컴퍼넌트의 문자열 변경
+				clockLable.setText(dateFormat.format(new Date()));
 				
 				try {
 					Thread.sleep(1000);
@@ -75,6 +87,7 @@ public class DigitalClockApp extends JFrame {
 		
 		
 		getContentPane().add(clockLable, BorderLayout.CENTER);
+		getContentPane().add(panel, BorderLayout.SOUTH);
 		
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setBounds(700, 200, 600, 200);
