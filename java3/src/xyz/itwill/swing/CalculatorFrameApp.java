6da09afad43ab9a3e30c3c9cd07b006881c4d198
itwill <itwill@DESKTOP-AFNN3T8>
2023-11-17 12:39:26 +0900

    22Day

--- java3/src/xyz/itwill/swing/CalculatorFrameApp.java
+++ java3/src/xyz/itwill/swing/CalculatorFrameApp.java
@@ -11,6 +11,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 //사칙 연산식을 입력받아 연산 결과를 출력하는 프로그램 작성
@@ -31,64 +32,6 @@ public class CalculatorFrameApp extends JFrame implements ActionListener {
 		super(title);
 		initButtons();
 		init();
-	}
-
-	private void init() {
-		label = new JLabel("0");
-		label.setFont(new Font("DIALOG", Font.BOLD, 30));
-		label.setHorizontalAlignment(JLabel.RIGHT);
-		label.setBackground(Color.LIGHT_GRAY);
-		label.setForeground(Color.WHITE);
-
-		JPanel p = new JPanel(new GridLayout(4, 4, 5, 5));
-		p.setBackground(Color.BLACK);
-		
-		p.add(bMulti);
-		p.add(bDiv);
-		p.add(bPlus);
-		p.add(bMinus);
-		p.add(b1);
-		p.add(b2);
-		p.add(b3);
-		p.add(b4);
-		p.add(b5);
-		p.add(b6);
-		p.add(b7);
-		p.add(b8);
-		p.add(b9);
-		p.add(b0);
-		p.add(bEquals);
-		p.add(bClear);
-		
-		Container container=getContentPane();
-		container.setLayout(new BorderLayout(10, 10));
-		container.setBackground(Color.BLACK);
-
-		container.add(label, BorderLayout.NORTH);
-		container.add(p, BorderLayout.CENTER);
-		
-		b0.addActionListener(this);
-		b1.addActionListener(this);
-		b2.addActionListener(this);
-		b3.addActionListener(this);
-		b4.addActionListener(this);
-		b5.addActionListener(this);
-		b6.addActionListener(this);
-		b7.addActionListener(this);
-		b8.addActionListener(this);
-		b9.addActionListener(this);
-		bDiv.addActionListener(this);
-		bPlus.addActionListener(this);
-		bMinus.addActionListener(this);
-		bMulti.addActionListener(this);
-		bClear.addActionListener(this);
-		bEquals.addActionListener(this);
-		
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-		setResizable(false);
-		setBounds(500, 100, 400, 400);
-		setVisible(true);
 	}
 
 	private void initButtons() {
@@ -143,6 +86,64 @@ public class CalculatorFrameApp extends JFrame implements ActionListener {
 		bClear.setBackground(Color.GREEN);
 		bEquals.setBackground(Color.CYAN);
 	}
+	
+	private void init() {
+		label = new JLabel("0");
+		label.setFont(new Font("DIALOG", Font.BOLD, 30));
+		label.setHorizontalAlignment(JLabel.RIGHT);
+		label.setBackground(Color.LIGHT_GRAY);
+		label.setForeground(Color.WHITE);
+
+		JPanel p = new JPanel(new GridLayout(4, 4, 5, 5));
+		p.setBackground(Color.BLACK);
+		
+		p.add(bMulti);
+		p.add(bDiv);
+		p.add(bPlus);
+		p.add(bMinus);
+		p.add(b1);
+		p.add(b2);
+		p.add(b3);
+		p.add(b4);
+		p.add(b5);
+		p.add(b6);
+		p.add(b7);
+		p.add(b8);
+		p.add(b9);
+		p.add(b0);
+		p.add(bEquals);
+		p.add(bClear);
+		
+		Container container=getContentPane();
+		container.setLayout(new BorderLayout(10, 10));
+		container.setBackground(Color.BLACK);
+
+		container.add(label, BorderLayout.NORTH);
+		container.add(p, BorderLayout.CENTER);
+		
+		b0.addActionListener(this);
+		b1.addActionListener(this);
+		b2.addActionListener(this);
+		b3.addActionListener(this);
+		b4.addActionListener(this);
+		b5.addActionListener(this);
+		b6.addActionListener(this);
+		b7.addActionListener(this);
+		b8.addActionListener(this);
+		b9.addActionListener(this);
+		bDiv.addActionListener(this);
+		bPlus.addActionListener(this);
+		bMinus.addActionListener(this);
+		bMulti.addActionListener(this);
+		bClear.addActionListener(this);
+		bEquals.addActionListener(this);
+		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		setResizable(false);
+		setBounds(500, 100, 500, 500);
+		setVisible(true);
+	}
 
 	public static void main(String[] args) {
 		new CalculatorFrameApp("계산기");
@@ -150,16 +151,91 @@ public class CalculatorFrameApp extends JFrame implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		//이벤트가 발생된 JButton 컴퍼넌트를 반환받아 저장
+		// => Event.getSource() 메소드는 이벤트를 발생된 컴퍼넌트를 Object 객체로 반환하므로
+		//JButton 컴퍼넌트의 메소드를 사용하기 위해서는 반드시 명시적 객체 형변환 필요
 		JButton eventSource=(JButton)e.getSource();
 		
+		//이벤트가 발생된 JButton 컴퍼넌트를 비교하여 명령 선택 실행
 		if(eventSource == bClear) {
-			
+			operation="";//연산식을 저장한 필드를 초기화 처리
+			label.setText("0");//JLable 컴퍼넌트의 출력값 초기화
 		} else if(eventSource == bEquals) {
+			//연산식에서 검색될 연산자가 저장된 문자열 배열 생성
+			String[] operatorArray={"*", "/", "+", "-"};
 			
+			//연산식에서 필요한 연산자를 검색하여 연산자의 시작첨자를 반환받아 저장
+			int index=-1;//연산식에서 연산자가 위치한 시작첨자를 저장하기 위한 변수
+			
+			//배열의 요소(연산자)의 차례대로 제공받아 반복 처리하는 반복문
+			for(String operator : operatorArray) {
+				//연산식에서 배열 요소에 저장된 연산자를 찾아 시작첨자를 반환받아 저장
+				//String.lastIndexOf(String str) : String 객체에 저장된 문자열에서 매개변수로 전달
+				//받은 문자열(문자)을 끝부터 차례대로 검색하여 시작위치값(첨자)를 반환하는 메소드
+				// => 매개변수로 전달받은 문자열을 찾을 수 없는 경우 [-1] 반환
+				index=operation.lastIndexOf(operator);
+
+				//연산식에서 연산자를 찾은 경우 반복문 종료
+				if(index != -1) break;
+			}
+			
+			//연산식에 연산자가 없는 경우 메소드 종료
+			if(index < 0) return;
+
+			try {
+				//연산식에서 첫번째 피연산자를 분리한 후 정수값으로 변환하여 저장
+				// => Integer.parseInt(String str) 메소드는 매개변수로 전달받은 문자열을 정수값으로
+				//변환하지 못할 경우 NumberFormatException 발생 - 예외 처리
+				int num1=Integer.parseInt(operation.substring(0, index));
+		
+				//연산식에서 연산자를 분리하여 저장
+				String operator=operation.substring(index, index+1);
+		
+				//연산식에서 두번째 피연산자를 분리한 후 정수값으로 변환하여 저장
+				int num2=Integer.parseInt(operation.substring(index+1));
+				
+				//연산자를 비교하여 피연산자에 대한 연산결과를 계산하여 저장
+				int result=0;//연산 결과를 저장하기 위한 변수
+				switch (operator) {
+				case "*": result=num1*num2; break;
+				//두번째 피연산자의 값이 [0]인 경우 ArithmeticException 예외 발생
+				case "/": result=num1/num2; break;
+				case "+": result=num1+num2; break;
+				case "-": result=num1-num2; break;
+				}
+				
+				//연산 결과값 출력
+				//label.setText(result+"");
+				label.setText(String.valueOf(result));
+				
+				//operation="";//연산식을 저장한 필드를 초기화 처리
+				operation=String.valueOf(result);//연산 결과값을 필드에 저장 - 반복 연산 가능 
+			} catch (ArithmeticException exception) {
+				label.setText("[에러]0으로 나눌 수 없습니다.");
+				operation="";
+			} catch (NumberFormatException exception) {
+				//JOptionPane 클래스 : 다이얼로그를 제공하는 클래스
+				//JOptionPane.showMessageDialog(Component parent, String message) : 메세지
+				//다이얼로그를 제공하여 출력하는 정적메소드
+				JOptionPane.showMessageDialog(this, "입력한 연산식이 형식에 맞지 않습니다.");
+			} catch (Exception exception) {
+				JOptionPane.showMessageDialog(this, "프로그램 실행에 예기치 못한 오류가 발생 되었습니다.");
+				System.exit(0);
+			}
 		} else {
+			//버튼의 문자열(라벨명)을 반환받아 operation 필드에 추가하여 저장
+			// => 버튼을 눌러 연산식을 완성하여 필드에 저장
 			operation+=eventSource.getText();
 			
+			//JLabel 컴퍼넌트의 문자열을 매개변수로 전달받은 operation 필드값(연산식)으로 변경
 			label.setText(operation);
 		}
 	}
 }
+
+
+
+
+
+
+
