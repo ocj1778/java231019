a3adf5a8811f2906569d0ccd3c63a53f88a63bd9
itwill <itwill@DESKTOP-AFNN3T8>
2023-11-17 12:07:31 +0900

    22Day

--- java3/src/xyz/itwill/swing/CalculatorFrameApp.java
+++ java3/src/xyz/itwill/swing/CalculatorFrameApp.java
@@ -13,15 +13,15 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-//사칙 연산식을 입력받아 연산 결과를 출력하는 프로그램
+//사칙 연산식을 입력받아 연산 결과를 출력하는 프로그램 작성
 public class CalculatorFrameApp extends JFrame implements ActionListener {
 	private static final long serialVersionUID = 1L;
 
-	//연산식을 입력하기 위한 필드(컴퍼넌트)
-	private JButton b_0, b_1, b_2, b_3, b_4, b_5, b_6, b_7, b_8, b_9, b_equals, b_plus,
-			b_minus, b_multi, b_div, b_C;
+	//연산식을 입력받기 위한 컴퍼넌트를 저장한 필드
+	private JButton b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, bEquals, bPlus,
+			bMinus, bMulti, bDiv, bClear;
 
-	//연산 결과를 출력하기 위한 필드(컴퍼넌트)
+	//연산 결과값을 출력하기 위한 컴퍼넌트를 저장한 필드
 	private JLabel label;
 
 	//연산식을 저장하기 위한 필드
@@ -43,45 +43,46 @@ public class CalculatorFrameApp extends JFrame implements ActionListener {
 		JPanel p = new JPanel(new GridLayout(4, 4, 5, 5));
 		p.setBackground(Color.BLACK);
 		
-		p.add(b_multi);
-		p.add(b_div);
-		p.add(b_plus);
-		p.add(b_minus);
-		p.add(b_1);
-		p.add(b_2);
-		p.add(b_3);
-		p.add(b_4);
-		p.add(b_5);
-		p.add(b_6);
-		p.add(b_7);
-		p.add(b_8);
-		p.add(b_9);
-		p.add(b_0);
-		p.add(b_equals);
-		p.add(b_C);
+		p.add(bMulti);
+		p.add(bDiv);
+		p.add(bPlus);
+		p.add(bMinus);
+		p.add(b1);
+		p.add(b2);
+		p.add(b3);
+		p.add(b4);
+		p.add(b5);
+		p.add(b6);
+		p.add(b7);
+		p.add(b8);
+		p.add(b9);
+		p.add(b0);
+		p.add(bEquals);
+		p.add(bClear);
 		
 		Container container=getContentPane();
 		container.setLayout(new BorderLayout(10, 10));
 		container.setBackground(Color.BLACK);
+
 		container.add(label, BorderLayout.NORTH);
 		container.add(p, BorderLayout.CENTER);
 		
-		b_0.addActionListener(this);
-		b_1.addActionListener(this);
-		b_2.addActionListener(this);
-		b_3.addActionListener(this);
-		b_4.addActionListener(this);
-		b_5.addActionListener(this);
-		b_6.addActionListener(this);
-		b_7.addActionListener(this);
-		b_8.addActionListener(this);
-		b_9.addActionListener(this);
-		b_div.addActionListener(this);
-		b_plus.addActionListener(this);
-		b_minus.addActionListener(this);
-		b_multi.addActionListener(this);
-		b_C.addActionListener(this);
-		b_equals.addActionListener(this);
+		b0.addActionListener(this);
+		b1.addActionListener(this);
+		b2.addActionListener(this);
+		b3.addActionListener(this);
+		b4.addActionListener(this);
+		b5.addActionListener(this);
+		b6.addActionListener(this);
+		b7.addActionListener(this);
+		b8.addActionListener(this);
+		b9.addActionListener(this);
+		bDiv.addActionListener(this);
+		bPlus.addActionListener(this);
+		bMinus.addActionListener(this);
+		bMulti.addActionListener(this);
+		bClear.addActionListener(this);
+		bEquals.addActionListener(this);
 		
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 
@@ -91,56 +92,56 @@ public class CalculatorFrameApp extends JFrame implements ActionListener {
 	}
 
 	private void initButtons() {
-		b_0 = new JButton("0");
-		b_1 = new JButton("1");
-		b_2 = new JButton("2");
-		b_3 = new JButton("3");
-		b_4 = new JButton("4");
-		b_5 = new JButton("5");
-		b_6 = new JButton("6");
-		b_7 = new JButton("7");
-		b_8 = new JButton("8");
-		b_9 = new JButton("9");
-		b_equals = new JButton("=");
-		b_plus = new JButton("+");
-		b_minus = new JButton("-");
-		b_multi = new JButton("*");
-		b_div = new JButton("/");
-		b_C = new JButton("C");
+		b0 = new JButton("0");
+		b1 = new JButton("1");
+		b2 = new JButton("2");
+		b3 = new JButton("3");
+		b4 = new JButton("4");
+		b5 = new JButton("5");
+		b6 = new JButton("6");
+		b7 = new JButton("7");
+		b8 = new JButton("8");
+		b9 = new JButton("9");
+		bEquals = new JButton("=");
+		bPlus = new JButton("+");
+		bMinus = new JButton("-");
+		bMulti = new JButton("*");
+		bDiv = new JButton("/");
+		bClear = new JButton("C");
 
-		b_0.setFont(new Font("DIALOG", Font.PLAIN, 20));
-		b_1.setFont(new Font("DIALOG", Font.PLAIN, 20));
-		b_2.setFont(new Font("DIALOG", Font.PLAIN, 20));
-		b_3.setFont(new Font("DIALOG", Font.PLAIN, 20));
-		b_4.setFont(new Font("DIALOG", Font.PLAIN, 20));
-		b_5.setFont(new Font("DIALOG", Font.PLAIN, 20));
-		b_6.setFont(new Font("DIALOG", Font.PLAIN, 20));
-		b_7.setFont(new Font("DIALOG", Font.PLAIN, 20));
-		b_8.setFont(new Font("DIALOG", Font.PLAIN, 20));
-		b_9.setFont(new Font("DIALOG", Font.PLAIN, 20));
-		b_div.setFont(new Font("DIALOG", Font.PLAIN, 20));
-		b_plus.setFont(new Font("DIALOG", Font.PLAIN, 20));
-		b_minus.setFont(new Font("DIALOG", Font.PLAIN, 20));
-		b_multi.setFont(new Font("DIALOG", Font.PLAIN, 20));
-		b_C.setFont(new Font("DIALOG", Font.PLAIN, 20));
-		b_equals.setFont(new Font("DIALOG", Font.PLAIN, 20));
+		b0.setFont(new Font("DIALOG", Font.PLAIN, 20));
+		b1.setFont(new Font("DIALOG", Font.PLAIN, 20));
+		b2.setFont(new Font("DIALOG", Font.PLAIN, 20));
+		b3.setFont(new Font("DIALOG", Font.PLAIN, 20));
+		b4.setFont(new Font("DIALOG", Font.PLAIN, 20));
+		b5.setFont(new Font("DIALOG", Font.PLAIN, 20));
+		b6.setFont(new Font("DIALOG", Font.PLAIN, 20));
+		b7.setFont(new Font("DIALOG", Font.PLAIN, 20));
+		b8.setFont(new Font("DIALOG", Font.PLAIN, 20));
+		b9.setFont(new Font("DIALOG", Font.PLAIN, 20));
+		bDiv.setFont(new Font("DIALOG", Font.PLAIN, 20));
+		bPlus.setFont(new Font("DIALOG", Font.PLAIN, 20));
+		bMinus.setFont(new Font("DIALOG", Font.PLAIN, 20));
+		bMulti.setFont(new Font("DIALOG", Font.PLAIN, 20));
+		bClear.setFont(new Font("DIALOG", Font.PLAIN, 20));
+		bEquals.setFont(new Font("DIALOG", Font.PLAIN, 20));
 		
-		b_0.setBackground(Color.WHITE);
-		b_1.setBackground(Color.WHITE);
-		b_2.setBackground(Color.WHITE);
-		b_3.setBackground(Color.WHITE);
-		b_4.setBackground(Color.WHITE);
-		b_5.setBackground(Color.WHITE);
-		b_6.setBackground(Color.WHITE);
-		b_7.setBackground(Color.WHITE);
-		b_8.setBackground(Color.WHITE);
-		b_9.setBackground(Color.WHITE);
-		b_div.setBackground(Color.YELLOW);
-		b_plus.setBackground(Color.YELLOW);
-		b_minus.setBackground(Color.YELLOW);
-		b_multi.setBackground(Color.YELLOW);
-		b_C.setBackground(Color.GREEN);
-		b_equals.setBackground(Color.CYAN);
+		b0.setBackground(Color.WHITE);
+		b1.setBackground(Color.WHITE);
+		b2.setBackground(Color.WHITE);
+		b3.setBackground(Color.WHITE);
+		b4.setBackground(Color.WHITE);
+		b5.setBackground(Color.WHITE);
+		b6.setBackground(Color.WHITE);
+		b7.setBackground(Color.WHITE);
+		b8.setBackground(Color.WHITE);
+		b9.setBackground(Color.WHITE);
+		bDiv.setBackground(Color.YELLOW);
+		bPlus.setBackground(Color.YELLOW);
+		bMinus.setBackground(Color.YELLOW);
+		bMulti.setBackground(Color.YELLOW);
+		bClear.setBackground(Color.GREEN);
+		bEquals.setBackground(Color.CYAN);
 	}
 
 	public static void main(String[] args) {
@@ -151,9 +152,9 @@ public class CalculatorFrameApp extends JFrame implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		JButton eventSource=(JButton)e.getSource();
 		
-		if(eventSource == b_C) {
+		if(eventSource == bClear) {
 			
-		} else if(eventSource == b_equals) {
+		} else if(eventSource == bEquals) {
 			
 		} else {
 			operation+=eventSource.getText();
