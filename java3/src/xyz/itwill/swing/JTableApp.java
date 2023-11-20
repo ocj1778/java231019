6def0fc89cd474458683f49561bc342eb90eeeb7
itwill <itwill@DESKTOP-AFNN3T8>
2023-11-20 13:22:01 +0900

    23Day

--- java3/src/xyz/itwill/swing/JTableApp.java
+++ java3/src/xyz/itwill/swing/JTableApp.java
@@ -2,16 +2,19 @@ package xyz.itwill.swing;
 
 import java.awt.BorderLayout;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Vector;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
 //학생정보를 관리하기 위한 프로그램 작성
@@ -31,7 +34,6 @@ public class JTableApp extends JFrame {
 		JPanel panel=new JPanel();
 		panel.add(addBtn);
 		panel.add(removeBtn);
-		
 		
 		//테이블의 컬럼명으로 사용될 String 배열 선언
 		String[] columnNames={"학번", "이름", "전화번호"};
@@ -76,9 +78,19 @@ public class JTableApp extends JFrame {
 			Object eventSource=e.getSource();
 			
 			if(eventSource == addBtn) {
+				//다이얼로그 출력
 				dialog.setVisible(true);
 			} else if(eventSource == removeBtn) {
+				DefaultTableModel tableModel=(DefaultTableModel)table.getModel();
+
+				if(tableModel.getRowCount()==0) return;//행이 하나도 없는 경우 메소드 종료
 				
+				//JTable.getSelectedRow() : 테이블에서 선택행의 행번호를 반환하는 메소드
+				int row=table.getSelectedRow();
+				if(row == -1) return;//선택행이 없는 경우 메소드 종료
+				
+				//DefaultTableModel.removeRow(int row) : 매개변수로 전달받은 행을 삭제하는 메소드
+				tableModel.removeRow(row);
 			}
 		}
 	}
@@ -87,6 +99,9 @@ public class JTableApp extends JFrame {
 	public class InputStudentDialog extends JDialog {
 		private static final long serialVersionUID = 1L;
 		
+		JTextField numTf, nameTf, phoneTf;
+		JButton okBtn, cancelBtn;
+		
 		//생성자 매개변수에는 부모창의 컨테이너 관련 객체와 제목을 전달받아 초기화
 		public InputStudentDialog(JFrame frame, String title) {
 			//JDialog(Frame owner, String title, boolean model) 생성자를 super 키워드로 호출
@@ -94,23 +109,74 @@ public class JTableApp extends JFrame {
 			//비활성화 여부를 설정하는 논리값(false : 부모창 활성화, true : 부모창 비활성화)을 
 			//전달받아 JDialog 객체 생성
 			super(frame, title, true);
+
+			getContentPane().setLayout(new GridLayout(2, 1));
+		
+			JPanel panelOne=new JPanel(new GridLayout(3, 2));
+
+			numTf=new JTextField();
+			nameTf=new JTextField();
+			phoneTf=new JTextField();
+			
+			panelOne.add(new JLabel("학번", JLabel.CENTER));
+			panelOne.add(numTf);
+			panelOne.add(new JLabel("이름", JLabel.CENTER));
+			panelOne.add(nameTf);
+			panelOne.add(new JLabel("전화번호", JLabel.CENTER));
+			panelOne.add(phoneTf);
+		
+			okBtn=new JButton("추가");	
+			cancelBtn=new JButton("취소");
+			
+			JPanel panelTwo=new JPanel();
+			panelTwo.add(okBtn);
+			panelTwo.add(cancelBtn);
+
+			getContentPane().add(panelOne); 
+			getContentPane().add(panelTwo); 
+			
+			okBtn.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					//JTextField 컴퍼넌트의 입력값을 반환받아 저장
+					String num=numTf.getText();
+					String name=nameTf.getText();
+					String phone=phoneTf.getText();
+
+					//Vector 객체를 생성하여 입력값을 요소로 추가하여 저장
+					Vector<String> vector=new Vector<String>();
+					vector.add(num);
+					vector.add(name);
+					vector.add(phone);
+					
+					//JTable.getModel() : 테이블 관련 행에 대한 정보가 저장된 TableModel 객체를
+					//반환하는 메소드
+					// => 명시적 객체 형변환을 이용하여 DefaultTableModel 객체로 형변환
+					DefaultTableModel tableModel=(DefaultTableModel)table.getModel();
+					
+					//DefaultTableModel.addRow(Vector vector) : Vector 객체된 요소값(컬럼값)으로
+					//테이블에 행을 추가하는 메소드
+					tableModel.addRow(vector);
+					
+					//JTextField 컴퍼넌트의 입력값 초기화
+					numTf.setText("");
+					nameTf.setText("");
+					phoneTf.setText("");
+					
+					setVisible(false);
+				}
+			});
+			
+			cancelBtn.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					setVisible(false);
+				}
+			});
 			
 			//[닫기] 버튼을 누르면 다이얼로그가 보여지지 않도록 설정
 			setDefaultCloseOperation(HIDE_ON_CLOSE);
-			
-			setBounds(700, 200, 400, 500);
+			setBounds(700, 200, 400, 300);
 		}
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
