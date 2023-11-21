af601901178280a865d1f0b55fbe0e80b1b6cce0
itwill <itwill@DESKTOP-AFNN3T8>
2023-11-21 16:07:40 +0900

    24Day

---
+++ java3/src/xyz/itwill/io/FileCopyByteApp.java
@@ -0,0 +1,9 @@
+package xyz.itwill.io;
+
+//원본파일(c:/data/bandzip.exe)에 저장된 값을 원시데이타로 제공받아 타겟파일(c:/data/bandzip_byte.exe)에
+//전달하여 저장하는 프로그램 작성 - 파일 복사 프로그램 
+public class FileCopyByteApp {
+	public static void main(String[] args) {
+		
+	}
+}
