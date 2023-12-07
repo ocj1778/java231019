51af3251f679e85ab22361405dfd1c18ec85f3f7
itwill <itwill@DESKTOP-AFNN3T8>
2023-12-07 14:55:39 +0900

    36Day

---
+++ jdbc/src/xyz/itwill/jdbc/InsertStudentApp.java
@@ -0,0 +1,39 @@
+package xyz.itwill.jdbc;
+
+//JDBC(Java DataBase Connectivity) : Java 프로그램을 사용하여 DBMS 서버에 접속해 SQL 명령을
+//전달하여 실행하기 위한 기능을 제공하는 Java API(인터페이스 또는 클래스)
+
+//java.sql : JDBC 기능을 프로그램을 작성하기 위한 Java 자료형이 선언된 패키지
+// => java.sql 패키지에는 JDBC 기능을 구현하기 위한 인터페이스 제공
+// => DBMS 종류가 다양하여 클래스 제공 불가능
+// => DBMS 서버 프로그램을 개발하여 관리하는 그룹에서 JDBC 기능을 제공하는 클래스(JDBC Driver)를
+//만들어 배포하므로 JDBC Driver 관련 라이브러리를 다운로드 받아 프로젝트에 빌드 처리
+
+
+//라이브러리 파일(Jar 파일)을 프로젝트에 빌드(연결) 처리해야 라이브러리의 인터페이스 또는 
+//클래스를 프로젝트에 작성된 클래스에서 사용 가능
+
+//Oracle DBMS 서버를 사용한 JDBC 프로그램을 작성하기 위해 Oracle JDBC Driver 관련 라이브러리를
+//빌드 처리하는 방법
+//1.https://www.oracle.com 사이트에서 Oracle JDBC Driver 관련 라이브러리 파일을 다운로드
+// => Oracle JDBC Driver 관련 라이브러리 파일 : ojdbc11.jar - JDK 버전 참고
+//2.Oracle JDBC Driver 관련 라이브러리 파일(ojdbc11.jar)을 프로젝트 폴더에 복사하여 붙여넣기
+
+
+public class InsertStudentApp {
+	public static void main(String[] args) {
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
