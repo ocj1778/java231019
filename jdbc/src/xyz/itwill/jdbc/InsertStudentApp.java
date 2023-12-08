a7d19ba629b38e5e52f3e9d28a4ddff15995f271
itwill <itwill@DESKTOP-AFNN3T8>
2023-12-08 10:29:14 +0900

    36Day

--- jdbc/src/xyz/itwill/jdbc/InsertStudentApp.java
+++ jdbc/src/xyz/itwill/jdbc/InsertStudentApp.java
@@ -8,19 +8,20 @@ import java.sql.Statement;
 //JDBC(Java DataBase Connectivity) : Java 프로그램을 사용하여 DBMS 서버에 접속해 SQL 명령을
 //전달하여 실행하기 위한 기능을 제공하는 Java API(인터페이스 또는 클래스)
 
-//java.sql : JDBC 기능을 프로그램을 작성하기 위한 Java 자료형이 선언된 패키지
+//java.sql : JDBC 프로그램을 작성하기 위한 Java 자료형이 선언된 패키지
 // => java.sql 패키지에는 JDBC 기능을 구현하기 위한 인터페이스 제공
-// => DBMS 종류가 다양하여 클래스 제공 불가능
-// => DBMS 서버 프로그램을 개발하여 관리하는 그룹에서 JDBC 기능을 제공하는 클래스(JDBC Driver)를
+// => DBMS 종류가 다양하여 JDK 라이브러리(jrt-fs.jar)에서 JDBC 관련 클래스를 제공 불가능
+// => DBMS 서버 프로그램을 개발하여 관리하는 그룹에서 JDBC 기능의 클래스(JDBC Driver)를
 //만들어 배포하므로 JDBC Driver 관련 라이브러리를 다운로드 받아 프로젝트에 빌드 처리
 
-//Oracle DBMS 서버를 사용한 JDBC 프로그램을 작성하기 위해 Oracle JDBC Driver 관련 라이브러리를
-//빌드 처리하는 방법
+//Oracle DBMS 서버에 접속하기 위한 JDBC 프로그램을 작성하기 위해 Oracle JDBC Driver 관련 
+//라이브러리를 빌드 처리하는 방법
 //1.https://www.oracle.com 사이트에서 Oracle JDBC Driver 관련 라이브러리 파일을 다운로드
 // => Oracle JDBC Driver 관련 라이브러리 파일 : ojdbc11.jar - JDK 버전 참고
 //2.Oracle JDBC Driver 관련 라이브러리 파일(ojdbc11.jar)을 프로젝트 폴더에 복사하여 붙여넣기
-//3.프로젝트 폴더에 저장된 라이브러리 파일을 프로젝트의 클래스에서 사용할 수 있도록 연결 - 빌드(Build) 
-// => 라이브러리 파일에 선언된 인터페이스 또는 클래스를 프로젝트의 클래스에서 사용할 수 있도록 설정
+//3.프로젝트 폴더에 저장된 라이브러리 파일을 프로젝트에 연결하여 프로그램 작성할 때 사용할 수 
+//있도록 빌드(Build) 처리 
+// => 라이브러리 파일에 작성된 인터페이스 또는 클래스를 프로젝트의 클래스에서 사용할 수 있도록 설정
 // => 프로젝트 >> 마우스 오른쪽 버튼 >> Properties >> Java Build Path >> Libraries >>
 //    classpath >> Add Jars >> 프로젝트 폴더의 Jar 파일을 선택 >> Apply And Close
 
