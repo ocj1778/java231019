c9b500ea187381744493392f3889d97185927b4e
itwill <itwill@DESKTOP-AFNN3T8>
2023-12-08 17:30:01 +0900

    37Day

--- jdbc/src/xyz/itwill/jdbc/ResultSetMetaDataApp.java
+++ jdbc/src/xyz/itwill/jdbc/ResultSetMetaDataApp.java
@@ -11,8 +11,8 @@ public class ResultSetMetaDataApp {
 		Connection con=ConnectionFactory.getConnection();
 		Statement stmt=con.createStatement();
 		
-		//String sql="select * from student order by no";
-		String sql="select no,name from student order by no";
+		String sql="select * from student order by no";
+		//String sql="select no,name from student order by no";
 		//ResultSet 객체 : SELECT 명령을 전달하여 실행된 검색결과(검색행)를 저장한 객체
 		ResultSet rs=stmt.executeQuery(sql);
 		
@@ -28,20 +28,34 @@ public class ResultSetMetaDataApp {
 		int columnCount=rsmd.getColumnCount();
 		System.out.println("검색행의 컬럼 갯수 = "+columnCount);
 		System.out.println("==============================================================");
+		for(int i=1;i<=columnCount;i++) {//검색대상의 갯수만큼 반복 처리
+			//ResultSetMetaData.getColumnLabel(int columnIndex) : 첨자위치(columnIndex)의
+			//컬럼명을 반환하는 메소드
+			String columnLabel=rsmd.getColumnLabel(i);
+			
+			//ResultSetMetaData.getColumnLabel(int columnIndex) : 첨자위치(columnIndex)의
+			//컬럼의 NULL 허용 유무값(0 또는 1)을 반환하는 메소드
+			int isNull=rsmd.isNullable(i);
+			String nullResult="NULL";
+			//ResultSetMetaData.columnNoNulls : NULL를 허용하지 않는 상수 - 정수값 : 0
+			if(isNull==ResultSetMetaData.columnNoNulls) {
+				nullResult="NOT NULL";
+			}
+			
+			//ResultSetMetaData.getColumnTypeName(int columnIndex) : 첨자위치(columnIndex)의
+			//컬럼의 오라클 자료형을 반환하는 메소드
+			String columnTypeName=rsmd.getColumnTypeName(i);
+			
+			//ResultSetMetaData.getColumnDisplaySize(int columnIndex) : 첨자위치(columnIndex)의
+			//컬럼의 출력크기를 반환하는 메소드
+			int columnDisplaySize=rsmd.getColumnDisplaySize(i);
+			
+			System.out.println("컬럼의 이름 = "+columnLabel);
+			System.out.println("컬럼의 NULL 허용 유무 = "+nullResult);
+			System.out.println("컬럼의 자료형 = "+columnTypeName);
+			System.out.println("컬럼의 출력크기 = "+columnDisplaySize);
+			System.out.println("==============================================================");
+		}
 		ConnectionFactory.close(con, stmt, rs);
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
