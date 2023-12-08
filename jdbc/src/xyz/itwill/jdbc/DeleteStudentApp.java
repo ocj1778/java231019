585fdf3d9fb5bbf7509542b224b078f1e7c7fbbe
itwill <itwill@DESKTOP-AFNN3T8>
2023-12-08 12:15:24 +0900

    37Day

--- jdbc/src/xyz/itwill/jdbc/DeleteStudentApp.java
+++ jdbc/src/xyz/itwill/jdbc/DeleteStudentApp.java
@@ -1,6 +1,45 @@
 package xyz.itwill.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 //STUDENT 테이블에 저장된 학생정보 중 학번이 [3000]인 학생정보를 삭제하는 JDBC 프로그램 작성
 public class DeleteStudentApp {
-
+	public static void main(String[] args) {
+		Connection con=null;
+		Statement stmt=null;
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+			String url="jdbc:oracle:thin:@localhost:1521:xe";
+			String username="scott";
+			String password="tiger";
+			con=DriverManager.getConnection(url, username, password);
+			
+			stmt=con.createStatement();
+			
+			String sql="delete from student where no=3000";
+			int rows=stmt.executeUpdate(sql);
+		
+			if(rows>0) {//삭제행이 있는 경우
+				System.out.println("[메세지]"+rows+"명의 학생정보를 삭제 하였습니다.");
+			} else {//삭제행이 없는 경우
+				System.out.println("[메세지]해당 학번의 학생정보를 찾을 수 없습니다.");
+			}
+		} catch (ClassNotFoundException e) {
+			System.out.println("[에러]OracleDriver 클래스를 찾을 수 없습니다.");
+		} catch (SQLException e) {
+			System.out.println("[에러]JDBC 관련 오류 = "+e.getMessage());
+		} finally {
+			try {
+				if(stmt!=null) stmt.close();
+				if(con!=null) con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
