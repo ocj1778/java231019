7a1f5baedf48e427f14c9ac56d3d3e7bb7bdbb66
itwill <itwill@DESKTOP-AFNN3T8>
2023-12-08 15:23:38 +0900

    37Day

---
+++ jdbc/src/xyz/itwill/jdbc/SelectEmpApp.java
@@ -0,0 +1,9 @@
+package xyz.itwill.jdbc;
+
+//EMP 테이블에 저장된 모든 사원의 사원번호,사원이름,급여를 급여로 내림차순 정렬되도록 검색하여
+//출력하는 JDBC 프로그램 작성
+public class SelectEmpApp {
+	public static void main(String[] args) {
+		
+	}
+}
