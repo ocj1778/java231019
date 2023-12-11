46dc24d4902c6384bbff1b660ee0c36ad922698f
itwill <itwill@DESKTOP-AFNN3T8>
2023-12-11 13:22:04 +0900

    38Day

--- jdbc/src/xyz/itwill/jdbc/ResultSetCursorApp.java
+++ jdbc/src/xyz/itwill/jdbc/ResultSetCursorApp.java
@@ -19,7 +19,7 @@ public class ResultSetCursorApp {
 		// => ResultSet 객체에 저장된 검색행은 행단위로 처리하기 위해 커서(Cursor) 사용
 		// => ResultSet 커서는 다음행으로만 이동 가능하며 커서 위치의 처리행 대한 조작 불가능
 		ResultSet rs=stmt.executeQuery(sql);
-		//System.out.println(rs);
+		//System.out.println("rs = "+rs);
 		
 		//ResultSet.next() : ResultSet 객체의 커서를 다음행으로 이동하는 메소드
 		// => 이동된 커서 위치에 처리행이 없는 경우 [false]를 반환하고 처리행이 있는 경우 [true] 반환
@@ -30,7 +30,73 @@ public class ResultSetCursorApp {
 		}
 		
 		System.out.println("==============================================================");
+		//Connection.createStatement(int resultSetType, int resultSetConcurrency)
+		// => 현재 접속중인 DBMS 서버에 SQL 명령을 전달하여 실행하기 위한 Statement 객체를 반환하는 메소드
+		// => 매개변수에 전달된 값에 따라 Statement 객체에 의해 생성되는 ResultSet 객체의
+		//커서에 대한 이동 설정 및 커서가 위치한 처리행의 조작 설정 가능
+		//resultSetType : ResultSet 커서의 이동 관련 속성값(ResultSet 인터페이스의 상수) 전달
+		// => ResultSet.TYPE_FORWARD_ONLY : ResultSet 커서를 다음행으로만 이동 가능 - 기본값
+		// => ResultSet.TYPE_SCROLL_INSENSITIVE : ResultSet 커서를 자유롭게 이동 가능 - 행에 대한 변경 미반영
+		// => ResultSet.TYPE_SCROLL_SENSITIVE : ResultSet 커서를 자유롭게 이동 가능 - 행에 대한 변경 반영
+		//resultSetConcurrency : ResultSet 커서가 위치한 처리행의 조작 관련 속성값(ResultSet 인터페이스의 상수) 전달
+		// => ResultSet.CONCUR_READ_ONLY : ResultSet 커서가 위치한 처리행의 조작 불가능 - 기본값
+		// => ResultSet.CONCUR_UPDATABLE : ResultSet 커서가 위치한 처리행의 조작 가능
+		stmt=con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 		
+		sql="select * from student order by no";
+		rs=stmt.executeQuery(sql);
+		//System.out.println("rs = "+rs);
+		
+		//ResultSet.first() : ResultSet 커서를 첫번째 행으로 이동하는 메소드
+		rs.first();
+		System.out.println(rs.getRow()+"행 : 학번 = "+rs.getInt("no")+", 이름 = "+rs.getString("name"));
+
+		//ResultSet.last() : ResultSet 커서를 마지막 행으로 이동하는 메소드
+		rs.last();
+		System.out.println(rs.getRow()+"행 : 학번 = "+rs.getInt("no")+", 이름 = "+rs.getString("name"));
+
+		//ResultSet.absolute(int index) : ResultSet 커서를 매개변수로 전달받은 행으로 이동하는 메소드
+		rs.absolute(2);
+		System.out.println(rs.getRow()+"행 : 학번 = "+rs.getInt("no")+", 이름 = "+rs.getString("name"));
+		System.out.println("==============================================================");
+		//ResultSet.afterLast() : ResultSet 커서를 EOF(End Of File) 영역으로 이동하는 메소드
+		rs.afterLast();	
+		//ResultSet.previous() : ResultSet 커서를 이전행으로 이동하는 메소드
+		while(rs.previous()) {
+			System.out.println(rs.getRow()+"행 : 학번 = "+rs.getInt("no")+", 이름 = "+rs.getString("name"));
+		}
+		System.out.println("==============================================================");
+		//ResultSet.beforeFirst() : ResultSet 커서를 BOF(Bofore Of File) 영역으로 이동하는 메소드
+		rs.beforeFirst();	
+		//ResultSet.next() : ResultSet 커서를 다음행으로 이동하는 메소드
+		while(rs.next()) {
+			System.out.println(rs.getRow()+"행 : 학번 = "+rs.getInt("no")+", 이름 = "+rs.getString("name"));
+		}
+		System.out.println("==============================================================");
+		stmt=con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+		
+		//ResultSet 커서가 위치한 처리행을 조작하기 위해서는 SELECT 명령의 검색대상으로 [*] 사용 불가능
+		sql="select no,name,phone,address,birthday from student order by no";
+		rs=stmt.executeQuery(sql);
+		//System.out.println("rs = "+rs);
+
+		rs.absolute(2);
+
+		//ResultSet.updateXXX(String columnLabel, XXX value) : ResultSet 커서가 위치한 처리행의
+		//컬럼값을 변경하는 메소드
+		// => XXX는 컬럼값을 변경하기 위해 전달받기 위한 Java 자료형
+		rs.updateString("name", "임걱정");
+		
+		//ResultSet.updateRow() : ResultSet 커서가 위치되어 컬럼값이 변경된 처리행을 테이블에
+		//적용하여 변경 처리하는 메소드 - UPDATE 명령을 전달하여 실행한 것과 동일한 결과 제공
+		rs.updateRow();
+		
+		rs.beforeFirst();	
+		while(rs.next()) {
+			System.out.println(rs.getRow()+"행 : 학번 = "+rs.getInt("no")+", 이름 = "+rs.getString("name"));
+		}
+		
+		System.out.println("==============================================================");
 		ConnectionFactory.close(con, stmt, rs);
 	}
 }
