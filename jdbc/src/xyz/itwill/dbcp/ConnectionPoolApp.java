1f658dcf00f79608309fc835debadac157fd732e
itwill <itwill@DESKTOP-AFNN3T8>
2023-12-12 12:42:51 +0900

    39Day

--- jdbc/src/xyz/itwill/dbcp/ConnectionPoolApp.java
+++ jdbc/src/xyz/itwill/dbcp/ConnectionPoolApp.java
@@ -1,7 +1,12 @@
 package xyz.itwill.dbcp;
 
-//DBCP
+//DBCP(DataBase Connection Pool) : 다수의 Connection 객체를 미리 생성하여 저장하기 위한 클래스
+// => Connection 객체가 미리 생성되어 있으므로 JDBC 프로그램의 실행 속도 증가
+// => Connection 객체를 생성하기 위한 정보의 변경 용이 - 생산성 및 유지보수의 효율성 증가
+// => Connection 객체의 갯수 제한 가능 - 자원의 낭비를 최소화
 
 public class ConnectionPoolApp {
-
+	public static void main(String[] args) {
+		
+	}
 }
