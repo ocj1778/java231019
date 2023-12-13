907dfe5c17214e750f1da4165cfd17b73da3b40f
itwill <itwill@DESKTOP-AFNN3T8>
2023-12-13 12:47:19 +0900

    40Day

--- jdbc/src/xyz/itwill/student/JdbcDAO.java
+++ jdbc/src/xyz/itwill/student/JdbcDAO.java
@@ -13,8 +13,7 @@ import oracle.ucp.jdbc.PoolDataSourceFactory;
 //객체로부터 Connection 객체를 제공받아 반환하는 메소드와 JDBC 관련 객체를 매개변수로 전달받아
 //삭제하는 메소드 작성
 // => 객체 생성이 목적이 아닌 상속을 목적으로 작성된 클래스이므로 추상클래스로 선언하는 것을 권장
-
-public class JdbcDAO {
+public abstract class JdbcDAO {
 	//DBCP 기능을 제공하기 위한 PoolDataSource 객체를 저장하기 위한 정적필드
 	private static PoolDataSource pds;
 	
