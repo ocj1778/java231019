893076947d782218e2e31b95c6a1a59a0dce8207
itwill <itwill@DESKTOP-AFNN3T8>
2024-01-12 16:45:26 +0900

    60Day

--- servlet/src/main/java/xyz/itwill/dao/StudentDAO.java
+++ servlet/src/main/java/xyz/itwill/dao/StudentDAO.java
@@ -15,7 +15,7 @@ import xyz.itwill.dto.StudentDTO;
 
 //STUDENT 테이블에 학생정보(행)을 삽입,삭제,변경,검색하는 기능을 제공하는 클래스
 // => DBMS 서버에 하나의 SQL 명령을 전달하여 실행하고 실행결과를 Java 객체로 매핑하여 반환하는 메소드 작성  
-public class StudentDAO {
+public class StudentDAO extends JdbcDAO {
 	private static StudentDAO _dao;
 	
 	private StudentDAO() {
@@ -42,25 +42,31 @@ public class StudentDAO {
 		ResultSet rs=null;
 		List<StudentDTO> studentList=new ArrayList<StudentDTO>();
 		try {
-			 
-		} catch (SQLException e) {
-			// TODO: handle exception
-		} finally {
+			con=getConnection();
 			
+			String sql="select no,name,phone,address,birthday from student order by no";
+			pstmt=con.prepareStatement(sql);
+			
+			rs=pstmt.executeQuery();
+			
+			//검색결과를 Java 객체(List 객체)로 매핑 처리
+			while(rs.next()) {
+				//DTO 객체를 생성하여 처리행의 컬럼값을 객체 필드에 저장
+				StudentDTO student=new StudentDTO();
+				student.setNo(rs.getInt("no"));
+				student.setName(rs.getString("name"));
+				student.setPhone(rs.getString("phone"));
+				student.setAddress(rs.getString("address"));
+				student.setBirthday(rs.getString("birthday"));
+				
+				//List 객체의 요소에 DTO 객체 추가
+				studentList.add(student);
+			}
+		} catch (SQLException e) {
+			System.out.println("[에러]selectStudentList() 메소드의 SQL 오류 = "+e.getMessage());
+		} finally {
+			close(con, pstmt, rs);
 		}
 		return studentList;
 	}
-	
 }
-
-
-
-
-
-
-
-
-
-
-
-
