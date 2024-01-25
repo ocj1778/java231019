891ba42a96e493b635e78aa18c3733654eb26207
itwill <itwill@DESKTOP-AFNN3T8>
2024-01-25 15:21:50 +0900

    69Day

---
+++ shop/src/main/java/xyz/itwill/dto/ReviewDTO.java
@@ -0,0 +1,14 @@
+package xyz.itwill.dto;
+
+/*
+create table review(review_num number primary key, review_member number constraint review_member_fk 
+    references member(member_num), review_subject varchar2(500), review_content varchar2(4000) 
+    , review_image varchar2(100), review_register date, review_update date, review_count number
+    , ref number, restep number, relevel number, reveiw_ip varchar2(20), review_status number(1));
+	    
+create sequence review_seq; 
+*/
+
+public class ReviewDTO {
+
+}
