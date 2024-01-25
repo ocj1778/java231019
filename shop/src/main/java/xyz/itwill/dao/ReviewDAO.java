0b6399ec400c6b59e35fb910b3c9993db2d33cec
itwill <itwill@DESKTOP-AFNN3T8>
2024-01-25 16:48:41 +0900

    69Day

---
+++ shop/src/main/java/xyz/itwill/dao/ReviewDAO.java
@@ -0,0 +1,73 @@
+package xyz.itwill.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ReviewDAO extends JdbcDAO {
+	private static ReviewDAO _dao;
+	
+	private ReviewDAO() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	static {
+		_dao=new ReviewDAO();		
+	}
+	
+	public static ReviewDAO getDAO() {
+		return _dao;
+	}
+	
+	//검색정보(검색대상과 검색단어)를 전달받아 REVIEW 테이블에 저장된 게시글 중 검색대상의 
+	//컬럼에 검색단어가 포함된 게시글의 갯수를 검색하여 반환하는 메소드
+	public int selectReviewSearchCount(String search, String keyword) {
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		int searchCount=0;
+		try {
+			con=getConnection();
+			
+			//매개변수에 저장된 값을 비교하여 접속된 DBMS 서버에 다른 SQL 명령을 전달하여 실행
+			// => 동적 SQL(Dynamic SQL)
+			if(keyword.equals("")) {//검색 기능을 사용하지 않은 경우
+				String sql="select count(*) from review";
+				pstmt=con.prepareStatement(sql);
+			} else {//검색 기능을 사용한 경우
+				String sql="select count(*) from review join member on review_member=member_num"
+						+ " where "+search+" like '%'||?||'%' and review_status<>0";
+				pstmt=con.prepareStatement(sql);
+				pstmt.setString(1, keyword);
+			}
+			
+			rs=pstmt.executeQuery();
+			
+			if(rs.next()) {
+				searchCount=rs.getInt(1);
+			}
+		} catch (SQLException e) {
+			System.out.println("[에러]selectReviewSearchCount() 메소드의 SQL 오류 = "+e.getMessage());
+		} finally {
+			close(con, pstmt, rs);
+		}
+		return searchCount;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- shop/src/main/webapp/review/review_list.jsp
+++ shop/src/main/webapp/review/review_list.jsp
@@ -1,3 +1,52 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<h1>제품후기 목록 페이지</h1>
+<%-- REVIEW 테이블에 저장된 행을 검색하여 게시글 목록을 전달하여 응답하는 JSP 문서 --%>
+<%-- => 게시글을 페이지 단위로 구분하여 검색해 출력 처리되도록 작성 - 페이징 처리 --%>
+<%-- => [페이지번호] 태그를 클릭한 경우 [/review/review_list.jsp] 문서를 요청하여 페이지 이동 
+- 페이지번호, 검색대상, 검색단어 전달 --%>
+<%-- => [검색] 태그를 클릭한 경우 [/review/review_list.jsp] 문서를 요청하여 페이지 이동 
+- 검색대상, 검색단어 전달 --%>
+<%-- => [글쓰기] 태그를 클릭한 경우 [/review/review_write.jsp] 문서를 요청하여 페이지 이동 
+- 로그인 상태의 사용자에게만 출력되도록 작성 --%>
+<%-- => 게시글의 [제목] 태그를 클릭한 경우 [/review/review_detail.jsp] 문서를 요청하여 페이지 이동 
+- 글번호, 페이지번호, 검색대상, 검색단어 전달 --%>
+<%
+	//게시글 검색 기능에 필요한 전달값(검색대상과 검색단어)을 반환받아 저장
+	String search=request.getParameter("search");//검색대상
+	if(search==null) {//전달값이 없는 경우 - 게시글 검색 기능을 사용하지 않은 경우
+		search="";
+	}
+	
+	String keyword=request.getParameter("keyword");//검색단어
+	if(keyword==null) {//전달값이 없는 경우
+		keyword="";
+	}
+
+	//페이징 처리에 필요한 전달값(페이지번호과 게시글갯수)을 반환받아 저장
+	int pageNum=1;//페이지번호- 전달값이 없는 경우 저장된 초기값 설정
+	if(request.getParameter("pageNum")!=null) {//전달값이 있는 경우
+		pageNum=Integer.parseInt(request.getParameter("pageNum"));
+	}
+	
+	int pageSize=10;//게시글갯수- 전달값이 없는 경우 저장된 초기값 설정
+	if(request.getParameter("pageSize")!=null) {//전달값이 있는 경우
+		pageNum=Integer.parseInt(request.getParameter("pageSize"));
+	}
+
+	//검색정보(검색대상과 검색단어)를 전달받아 REVIEW 테이블에 저장된 게시글 중 검색대상의 컬럼에
+	//검색단어가 포함된 게시글의 갯수를 검색하여 반환하는 ReviewDAO 클래스의 메서드 호출
+	
+%>
+
+
+
+
+
+
+
+
+
+
+
+
+
