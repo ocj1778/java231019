a33b9b275acf818fd06c9c99f919a37314cfd606
itwill <itwill@DESKTOP-AFNN3T8>
2024-02-05 16:50:26 +0900

    76Day

---
+++ ajax/src/main/java/xyz/itwill/dto/SuggestDTO.java
@@ -0,0 +1,23 @@
+package xyz.itwill.dto;
+
+/*
+create table suggest(word varchar2(50) primary key, url varchar2(100));
+
+insert into suggest values('JAVA','https://www.oracle.com');
+insert into suggest values('Spring','https://spring.io');
+insert into suggest values('Servlet','https://www.servlets.com');
+insert into suggest values('eclise','https://www.eclipse.org');
+insert into suggest values('jQuery','https://www.jquery.com');
+insert into suggest values('apache','https://www.apache.org');
+insert into suggest values('tomcat','https://tomcat.apache.org');
+insert into suggest values('다음','https://www.daum.net');
+insert into suggest values('네이버','https://www.naver.com');
+insert into suggest values('구글','https://www.google.com');
+insert into suggest values('아이티윌','http://www.itwill.xyz');
+    
+commit;  
+*/
+
+public class SuggestDTO {
+
+}
--- ajax/src/main/webapp/jdbc/suggest_two.jsp
+++ ajax/src/main/webapp/jdbc/suggest_two.jsp
@@ -1,12 +1,11 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
+<?xml version="1.0" encoding="utf-8"?>
+<%@ page language="java" contentType="text/xml; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
-</head>
-<body>
+<%-- 검색어를 전달받아 SUGGEST 테이블에 저장된 행(제시어 관련 정보)를 검색하여 XML 데이타로 응답하는 JSP 문서 --%>
+<%
+	request.setCharacterEncoding("utf-8");
 
-</body>
-</html>
+	String keyword=request.getParameter("keyword");
+	
+	//검색어를 전달받아 SUGGEST 테이블에 저장된 행을 검색하여 List 객체로 반환하는 SuggestDAO 클래스의 메소드 호출
+%>    
