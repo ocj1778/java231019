e596ea9887bbda602e4d1d7692a48ea840d50aa9
itwill <itwill@DESKTOP-AFNN3T8>
2024-02-05 16:53:38 +0900

    76Day

--- ajax/src/main/java/xyz/itwill/dto/SuggestDTO.java
+++ ajax/src/main/java/xyz/itwill/dto/SuggestDTO.java
@@ -19,5 +19,26 @@ commit;
 */
 
 public class SuggestDTO {
+	private String word;
+	private String url;
+	
+	public SuggestDTO() {
+		// TODO Auto-generated constructor stub
+	}
 
+	public String getWord() {
+		return word;
+	}
+
+	public void setWord(String word) {
+		this.word = word;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
 }
