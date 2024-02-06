21506618c457781c01d7d79b9014b65582c51e8f
itwill <itwill@DESKTOP-AFNN3T8>
2024-02-06 11:47:21 +0900

    77Day

--- ajax/src/main/java/xyz/itwill/dto/AjaxCommentDTO.java
+++ ajax/src/main/java/xyz/itwill/dto/AjaxCommentDTO.java
@@ -6,5 +6,44 @@ package xyz.itwill.dto;
 //create sequence ajax_comment_seq;
   
 public class AjaxCommentDTO {
+	private int num;
+	private String writer;
+	private String content;
+	private String regdate;
+	
+	public AjaxCommentDTO() {
+		// TODO Auto-generated constructor stub
+	}
 
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getRegdate() {
+		return regdate;
+	}
+
+	public void setRegdate(String regdate) {
+		this.regdate = regdate;
+	}
 }
