bde166e8be6cb0d8acd387519ba61e76ff4c87d0
itwill <itwill@DESKTOP-AFNN3T8>
2024-02-21 15:20:31 +0900

    80Day

---
+++ mvc/src/main/java/xyz/itwill/mvc/ControllerServlet.java
@@ -0,0 +1,30 @@
+package xyz.itwill.mvc;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//컨트롤러(Controller - Servlet) : 클라이언트의 모든 요청을 받아 모델(Model - Class) 역활의
+//객체로 요청 처리 메소드를 호출하여 클라이언트의 요청을 처리하고 처리결과를 뷰(View - JSP)에게
+//제공하여 응답되도록 프로그램의 흐름을 제어하는 웹프로그램(서블릿)
+
+//1.클라이언트의 모든 요청을 받을 수 있도록 서블릿의 URL 패턴을 설정하여 단일 진입점의 기능 구현
+// => Front Controller Pattern
+//@WebServlet("url") : 클래스를 서블릿(웹프로그램)으로 등록하고 요청 URL 주소를 매핑하는 어노테이션
+// => 매핑 설정된 URL 주소에 패턴문자(* : 전체, ? : 문자 하나)를 사용하여 URL 패턴 등록 가능
+// => @WebServlet("*.do") : 클라이언트가 [XXX.do] 형식의 URL 주소로 요청한 경우 서블릿 실행
+// => @WebServlet 어노테이션 대신 [web.xml] 파일의 엘리먼트를 사용하여 클래스를 서블릿으로 
+//등록하고 URL 패턴을 매칭 처리 가능
+//@WebServlet("*.do")
+public class ControllerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+}
