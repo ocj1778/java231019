6fed5fd858ead33405636e43fad43c66b1b6230a
itwill <itwill@DESKTOP-AFNN3T8>
2024-03-04 12:31:13 +0900

    87Day

---
+++ mybatis/src/main/java/xyz/itwill/dto/MyMember.java
@@ -0,0 +1,50 @@
+package xyz.itwill.dto;
+
+//create table mymember(id varchar2(50) primary key, name varchar2(50)
+//	    , phone varchar2(20), email varchar2(100));
+
+//테이블 생성 >> DTO 클래스 작성 >> mybatis 환경설정파일에 별칭 및 맵퍼 등록 
+// >> 매퍼 파일 작성 >> DAO 클래스 작성
+
+public class MyMember {
+	private String id;
+	private String name;
+	private String phone;
+	private String email;
+	
+	public MyMember() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
