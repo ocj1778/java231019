897ed1c252c9d175faddca2cad817e567beb598c
itwill <itwill@DESKTOP-AFNN3T8>
2024-03-04 11:36:52 +0900

    86Day

--- mybatis/src/main/java/xyz/itwill/dao/StudentDAO.java
+++ mybatis/src/main/java/xyz/itwill/dao/StudentDAO.java
@@ -63,7 +63,7 @@ public class StudentDAO {
 			//전달하여 실행하고 실행결과를 List 객체로 반환하는 메소드
 			// => elementId 매개변수에는 매퍼 식별자(mapper 엘리먼트의 namespace 속성값)와  
 			//select 엘리먼트의 식별자(id 속성값)를 이용한 문자열을 전달받아 SQL 명령을 
-			//제공받아 DBMS 서버에 전달하여 실행
+			//제공해 DBMS 서버에 전달하여 실행
 			return sqlSession.selectList("xyz.itwill.mapper.StudentMapper.selectStudentList");
 		} finally {
 			sqlSession.close();
