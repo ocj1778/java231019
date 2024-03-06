e22c734e25fb4fd140d8d3a75a72292dd08c9cdf
itwill <itwill@DESKTOP-AFNN3T8>
2024-03-06 11:03:46 +0900

    88Day

--- mybatis/src/main/java/xyz/itwill/dto/MyUser.java
+++ mybatis/src/main/java/xyz/itwill/dto/MyUser.java
@@ -4,7 +4,6 @@ package xyz.itwill.dto;
 MYUSER 테이블 : 회원정보를 저정하기 위한 테이블
  => SQL 명령은 대소문자를 구분하지 않기 때문에 식별자를 선언할 때 스네이크 표기법 사용
  => 스네이크 표기법(SnakeCase) : 단어와 단어를 구분하기 위해 _ 기호를 사용하여 식별자를 선언하는 방법
-
 create table myuser(user_id varchar2(50) primary key, user_name varchar2(50));
 
 이름        널?       유형           
