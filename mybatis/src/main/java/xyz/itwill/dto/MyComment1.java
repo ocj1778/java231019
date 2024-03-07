7e91621e09c2838d310fd3c76e903fad5ba8121b
itwill <itwill@DESKTOP-AFNN3T8>
2024-03-07 16:32:24 +0900

    90Day

--- mybatis/src/main/java/xyz/itwill/dto/MyComment1.java
+++ mybatis/src/main/java/xyz/itwill/dto/MyComment1.java
@@ -1,5 +1,7 @@
 package xyz.itwill.dto;
 
+import java.io.Serializable;
+
 /*
 MYCOMMENT 테이블 : 게시글을 저장하기 위한 테이블
 create table mycomment(comment_no number primary key, comment_id varchar2(50)
@@ -18,7 +20,14 @@ COMMENT_DATE             DATE          - 게시글 작성일
 
 //테이블의 컬럼명과 같은 이름으로 클래스의 필드명을 작성 - 자동 매핑
 // => 스네이크 표기법으로 작성된 컬럼명은 카멜 표기법으로 자동 변경되므로 필드명은 카멜 표기법으로 작성
-public class MyComment1 {
+//XML 기반의 매퍼 파일에서 cache 엘리먼트를 사용한 경우 SELECT 명령으로 생성되는 객체의 클래스는
+//반드시 객체 직렬화 클래스로 선언해야만 객체 단위로 입출력 가능
+//객체 직렬화 클래스 : 객체 단위로 입력 또는 출력 처리되도록 작성된 클래스
+// => Serializable 인터페이스를 상속받아 작성
+// => serialVersionUID 이름의 static final 필드를 선언하는 것을 권장
+public class MyComment1 implements Serializable {
+	private static final long serialVersionUID = -7534009978055893003L;
+
 	private int commentNo;
 	private String commentId;
 	private String commentContent;
---
+++ mybatis/src/main/java/xyz/itwill/dto/MyHewon.java
@@ -0,0 +1,78 @@
+package xyz.itwill.dto;
+
+/*
+MYHEWON : 회원정보를 저장하기 위한 테이블
+create table myhewon(hewon_id varchar2(50) primary key, hewon_name varchar2(50)
+    , hewon_phone varchar2(20), hewon_email varchar2(100), hewon_status number(1));
+    
+이름           널?       유형            
+------------ -------- ------------- 
+HEWON_ID     NOT NULL VARCHAR2(50)  - 아이디 
+HEWON_NAME            VARCHAR2(50)  - 이름
+HEWON_PHONE           VARCHAR2(20)  - 전화번호
+HEWON_EMAIL           VARCHAR2(100) - 이메일
+HEWON_STATUS          NUMBER(1)     - 공개범위
+
+공개범위 : 1(아이디), 2(아이디&이름), 3(아이디&이름&전화번호), 4(아이디&이름&전화번호&이메일)
+*/
+
+public class MyHewon {
+	private String id;
+	private String name;
+	private String phone;
+	private String email;
+	private String status;
+	
+	public MyHewon() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public MyHewon(String id, String name, String phone, String email, String status) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.phone = phone;
+		this.email = email;
+		this.status = status;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
