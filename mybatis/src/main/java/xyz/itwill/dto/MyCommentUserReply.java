f338b32f5abd27586b3ee1a5300a6ecf09ad6803
itwill <itwill@DESKTOP-AFNN3T8>
2024-03-08 10:56:46 +0900

    90Day

--- mybatis/src/main/java/xyz/itwill/dto/MyCommentUserReply.java
+++ mybatis/src/main/java/xyz/itwill/dto/MyCommentUserReply.java
@@ -5,7 +5,7 @@ import java.util.List;
 //MYCOMMENT 테이블과 MYSUER 테이블에 저장된 행을 1:1 관계로 결합하고 MYCOMMENT 테이블과
 //MYREPLY 테이블(MYUSER 테이블)을 1:N 관계로 결합하여 검색한 결과값을 저장하기 위한 클래스
 public class MyCommentUserReply {
-	//MYCOMMENT 테이블의 검색행(게시글정보) 1개를 객체로 제공받아 저장하기 위한 필드
+	//MYCOMMENT 테이블의 검색행(게시글정보)에 컬럼값을 저장하기 위한 필드
 	private int commentNo;
 	private String commentId;
 	private String commentContent;
