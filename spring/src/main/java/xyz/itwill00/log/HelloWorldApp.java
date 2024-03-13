add319a29b22b46d447e2eaa5e6a653e2a09795a
itwill <itwill@DESKTOP-AFNN3T8>
2024-03-13 11:28:53 +0900

    93Day

--- spring/src/main/java/xyz/itwill00/log/HelloWorldApp.java
+++ spring/src/main/java/xyz/itwill00/log/HelloWorldApp.java
@@ -2,10 +2,10 @@ package xyz.itwill00.log;
 
 public class HelloWorldApp {
 	public static void main(String[] args) {
-		System.out.println("HelloWorldApp 클래스의 hello 메소드 - 시작");
+		System.out.println("HelloWorldApp 클래스의 main 메소드 - 시작");
 		HelloWorld hw=new HelloWorld();
 		String message=hw.hello("홍길동");
 		System.out.println("message = "+message);
-		System.out.println("HelloWorldApp 클래스의 hello 메소드 - 종료");
+		System.out.println("HelloWorldApp 클래스의 main 메소드 - 종료");
 	}
 }
