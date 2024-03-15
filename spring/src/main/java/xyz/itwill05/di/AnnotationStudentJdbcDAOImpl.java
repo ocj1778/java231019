ddc9d81fb6e61f0a8c225343fe4b94e5b6076dc3
itwill <itwill@DESKTOP-AFNN3T8>
2024-03-15 16:30:05 +0900

    96Day

---
+++ spring/src/main/java/xyz/itwill05/di/AnnotationStudentJdbcDAOImpl.java
@@ -0,0 +1,39 @@
+package xyz.itwill05.di;
+
+import java.util.List;
+
+public class AnnotationStudentJdbcDAOImpl implements StudentDAO {
+	public AnnotationStudentJdbcDAOImpl() {
+		System.out.println("### AnnotationStudentJdbcDAOImpl 클래스의 기본 생성자 호출 ###");
+	}
+	
+	@Override
+	public int insertStudent(Student student) {
+		System.out.println("*** AnnotationStudentJdbcDAOImpl 클래스의 insertStudent(Student student) 메소드 호출 ***");
+		return 0;
+	}
+
+	@Override
+	public int updateStudent(Student student) {
+		System.out.println("*** AnnotationStudentJdbcDAOImpl 클래스의 updateStudent(Student student) 메소드 호출 ***");
+		return 0;
+	}
+
+	@Override
+	public int deleteStudent(int num) {
+		System.out.println("*** AnnotationStudentJdbcDAOImpl 클래스의 deleteStudent(int num) 메소드 호출 ***");
+		return 0;
+	}
+
+	@Override
+	public Student selectStudent(int num) {
+		System.out.println("*** AnnotationStudentJdbcDAOImpl 클래스의 selectStudent(int num) 메소드 호출 ***");
+		return null;
+	}
+
+	@Override
+	public List<Student> selectStudentList() {
+		System.out.println("*** AnnotationStudentJdbcDAOImpl 클래스의 selectStudentList() 메소드 호출 ***");
+		return null;
+	}
+}
