94acaeae29705802ad6889dd18a5a9047d8f9bd4
itwill <itwill@DESKTOP-AFNN3T8>
2024-03-15 10:55:11 +0900

    95Day

--- spring/src/main/java/xyz/itwill04/bean/FactoryMethodBean.java
+++ spring/src/main/java/xyz/itwill04/bean/FactoryMethodBean.java
@@ -1,7 +1,7 @@
 package xyz.itwill04.bean;
 
 //싱글톤 디자인 패턴을 적용하여 작성된 클래스 - 싱글톤 클래스(Singleton Class)
-// => 프로그램에 필요한 객체를 하나만 제공하기 위한 목적의 클래스를 작성하기 위한 사용하는 디자인 패턴
+// => 싱글톤 클래스 : 프로그램에 필요한 객체를 하나만 제공하기 위한 목적으로 작성된 클래스
 public class FactoryMethodBean {
 	private static FactoryMethodBean _bean;
 	
