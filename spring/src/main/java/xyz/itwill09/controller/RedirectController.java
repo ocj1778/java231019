62830b2cd24b574245e97f9671716c3bbd6e98f9
itwill <itwill@DESKTOP-AFNN3T8>
2024-03-26 15:41:04 +0900

    108Day

--- spring/src/main/java/xyz/itwill09/controller/RedirectController.java
+++ spring/src/main/java/xyz/itwill09/controller/RedirectController.java
@@ -3,6 +3,7 @@ package xyz.itwill09.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class RedirectController {
@@ -13,21 +14,42 @@ public class RedirectController {
 		
 		//뷰(View)를 생성하기 위한 뷰이름(ViewName) 반환
 		// => Front Controller(DispatchServlet 객체)는 반환받은 뷰이름을 InternalResourceViewResolver
-		//객체에게 전달하여 JSP 문서의 경로를 제공받아 포워드 이동해 JSP 문서로 응답 처리
-		//포워드(Forward) : 클라이언트 요청에 의해 실행된 웹프로그램(Front Controller)에서 응답 
-		//처리할 웹프로그램(View - JSP)으로 스레드를 이동하여 명령을 실행
-		// => 클라이언트의 URL 주소는 변경되지 않으면 Request Scope 속성값을 객체로 제공받아 사용
+		//객체에게 전달하여 JSP 문서의 경로를 제공받아 스레드를 이동해 JSP 문서로 응답 처리 - 포워드 이동
+		//포워드 이동 : 클라이언트 요청에 의해 실행된 웹프로그램(Front Controller)에서 응답 
+		//처리할 웹프로그램(View - JSP)으로 스레드를 이동하여 응답 처리
+		// => 클라이언트의 URL 주소는 변경되지 않으면 Request Scope 속성값을 객체로 제공받아 사용 가능
 		return "display_forward";
 	}
 	
+	/*
+	@RequestMapping("/redirect")
+	public String redirect() {
+		//요청 처리 메소드의 반환값(뷰이름)에 redirect 접두사를 사용하여 반환하면 Front Controller
+		//(DispatchServlet 객체)는 반환받은 URL 주소를 클라이언트에게 전달하여 응답 처리
+		// => URL 주소를 응답받은 클라이언트는 브라우저 요청 URL 주소를 변경하여 해당 페이지를
+		//요청해 새로운 응답결과를 제공받아 출력 처리 - 리다이렉트 이동
+		//리다이렉트 이동 : 클라이언트에게 URL 주소를 전달하여 페이지를 재요청해 실행결과를 응답받아 처리
+		// => 클라이언트의 URL 주소는 변경되지만 Request Scope 속성값을 객체로 제공받아 사용 불가능
+		return "redirect:/redirect_move";
+	}
 	
+	*/
+
+	//요청 처리 메소드에 RedirectAttributes 인터페이스를 매개변수를 작성하여 RedirectAttributes
+	//객체를 제공받아 사용 가능
+	// => RedirectAttributes 객체 : 리다이렉트 이동되는 페이지의 요청 처리 메소드와 뷰에서
+	//사용할 수 있는 Request Scope 속성값을 제공하기 위한 객체
+	@RequestMapping("/redirect")
+	public String redirect(RedirectAttributes attributes) {
+		//RedirectAttributes.addFlashAttribute(String attributeName, Object attributeValue)
+		// => Request Scope 속성값을 저장하여 리다이렉트 이동되는 페이지의 요청 처리 메소드와
+		//뷰에게 속성값을 제공하기 위한 메소드
+		attributes.addFlashAttribute("name", "임꺽정");
+		return "redirect:/redirect_move";
+	}
+
+	@RequestMapping("/redirect_move")
+	public String redirect() {
+		return "display_redirect";
+	}
 }
-
-
-
-
-
-
-
-
-
---
+++ spring/src/main/webapp/WEB-INF/views/display_redirect.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>SPRING</title>
+</head>
+<body>
+	<h1>리다이렉트 이동</h1>
+	<hr>
+	<h2>${name }님, 환영합니다.</h2>
+</body>
+</html>
