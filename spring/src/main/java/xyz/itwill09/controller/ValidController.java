fc50cd0033cef2c2c3151a191dbb5c57af2845f0
itwill <itwill@DESKTOP-AFNN3T8>
2024-03-27 13:21:56 +0900

    109Day

--- spring/src/main/java/xyz/itwill09/controller/ValidController.java
+++ spring/src/main/java/xyz/itwill09/controller/ValidController.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -12,6 +14,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import xyz.itwill09.dto.Employee;
+
+//Command 객체의 필드에 저장될 전달값 검증 - Spring Validation
+//1.validation-api 라이브러리와 hibernate-validator 라이브러리를 프로젝트에 빌드 처리 - 메이븐 : pom.xml
+//2.HTML 태그 대신 Spring 태그를 사용하여 뷰(View - JSP) 작성 - 에러메세지를 제공받아 출력 처리
+//3.Controller 클래스의 요청 처리 메소드에서 전달값이 저장된 Command 객체가 저장될 매개변수에
+//@Valid 어노테이션을 사용하여 검증 처리되도록 설정
+//4.Command 객체에 대한 DTO 클래스의 필드에 검증 관련 어노테이션을 사용하여 전달값 검증
 
 @Controller
 //@RequestMapping 어노테이션을 클래스에 선언된 Controller 클래스의 모든 요청 처리 메소드와
@@ -55,7 +64,7 @@ public class ValidController {
 	//요청 처리 메소드의 매개변수를 Errors 인터페이스로 작성하면 Errors 객체를 전달받아 저장
 	// => Errors : 전달값에 대한 검증 실패시 발생되는 모든 에러 관련 정보를 저장한 객체
 	@RequestMapping(value = "/spring", method = RequestMethod.POST)
-	public String spring(@ModelAttribute Employee employee, Errors errors) {
+	public String spring(@ModelAttribute @Valid Employee employee, Errors errors) {
 		//Errors.hasErrors() : Errors 객체에 에러 관련 정보가 저장된 경우 [true]를 반환하는 메소드
 		if(errors.hasErrors()) {
 			//model.addAttribute("genderList", Arrays.asList("남자","여자"));
--- spring/src/main/java/xyz/itwill09/dto/Employee.java
+++ spring/src/main/java/xyz/itwill09/dto/Employee.java
@@ -1,12 +1,45 @@
 package xyz.itwill09.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 import lombok.Data;
 
 @Data
 public class Employee {
+	//@NotNull : 전달값이 [null]인 경우 에러를 발생하는 어노테이션
+	//@NotBlank : 전달값이 [null]이거나 전달값에 공백이 있는 경우 에러를 발생하는 어노테이션
+	//@NotEmpty : 전달값이 [null]이거나 [""]인 경우 에러를 발생하는 어노테이션
+	//message 속성 : 뷰에게 제공될 에러메세지를 속성값으로 설정
+	// => message 속성을 생략한 경우 기본적으로 설정된 에러메세지 제공
+	//@NotEmpty(message = "아이디를 반드시 입력해 주세요.")
+	//@Size : 전달값의 크기(문자갯수)를 비교하여 에러를 발생하는 어노테이션
+	//min 속성 : 전달값의 최소 크기를 속성값으로 설정
+	//max 속성 : 전달값의 최대 크기를 속성값으로 설정
+	//@Size(min = 6, max = 20, message = "아이디는 최소 6자 이상 최대 20자 이하로만 입력해 주세요.")
+	@Pattern(regexp = "^[a-zA-Z]\\w{5,19}$", message = "아이디를 형식에 맞게 입력해 주세요.")
 	private String id;
+	//@NotEmpty(message = "비밀번호를 입력해 주세요.")
+	@Pattern(regexp = "(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[~!@#$%^&*_-]).{6,20}$", message = "비밀번호를 형식에 맞게 입력해 주세요.")
 	private String passwd;
+	@NotEmpty(message = "이름을 입력해 주세요.")
 	private String name;
+	@NotEmpty(message = "이메일을 입력해 주세요.")
+	//@Pattern(regexp = "^([a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+(\\.[-a-zA-Z0-9]+)+)*$", message = "이메일을 형식에 맞게 입력해 주세요.")
+	//@Email : 이메일 형식이 틀린 경우 에러를 발생하는 어노테이션
+	@Email(message = "이메일을 형식에 맞게 입력해 주세요.")
 	private String email;
+	@NotEmpty(message = "성별을 입력해 주세요.")
 	private String gender;
 }
+
+
+
+
+
+
+
+
+
+
