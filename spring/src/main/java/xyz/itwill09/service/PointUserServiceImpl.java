30a49da19055ded8b99613976738583de48e0a77
itwill <itwill@DESKTOP-AFNN3T8>
2024-04-01 11:53:12 +0900

    112Day

--- spring/src/main/java/xyz/itwill09/service/PointUserServiceImpl.java
+++ spring/src/main/java/xyz/itwill09/service/PointUserServiceImpl.java
@@ -15,8 +15,23 @@ public class PointUserServiceImpl implements PointUserService {
 	//검색하여 DTO 객체로 반환하는 메소드
 	@Override
 	public PointUser addPointUser(PointUser user) {
+		//매개변수로 전달받은 회원정보의 아이디가 중복될 경우 인위적 예외 발생
+		if(pointUserDAO.selectPointUser(user.getId()) != null) {
+			throw new RuntimeException("이미 사용중인 아이디입니다.");
+		}
+		
 		pointUserDAO.insertPointUser(user);
 		return pointUserDAO.selectPointUser(user.getId());
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
---
+++ spring/src/test/java/xyz/itwill/spring/PointUserServiceTest.java
@@ -0,0 +1,40 @@
+package xyz.itwill.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import lombok.extern.slf4j.Slf4j;
+import xyz.itwill09.dto.PointUser;
+import xyz.itwill09.service.PointUserService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/*.xml"})
+@Slf4j
+public class PointUserServiceTest {
+	@Autowired
+	private PointUserService pointUserService;
+	
+	@Test
+	public void testAddPointUser() {
+		PointUser user=PointUser.builder().id("abc123").name("홍길동").build();
+		
+		PointUser addUser=pointUserService.addPointUser(user);
+		
+		log.info(addUser.toString());
+	}
+}
+
+
+
+
+
+
+
+
+
+
