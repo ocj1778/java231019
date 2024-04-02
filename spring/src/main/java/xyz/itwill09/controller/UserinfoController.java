0b3bb3d87bd1fdf095d24da05bdecf1afda9a8b9
itwill <itwill@DESKTOP-AFNN3T8>
2024-04-02 12:25:29 +0900

    113Day

--- spring/src/main/java/xyz/itwill09/controller/UserinfoController.java
+++ spring/src/main/java/xyz/itwill09/controller/UserinfoController.java
@@ -1,10 +1,18 @@
 package xyz.itwill09.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import lombok.RequiredArgsConstructor;
+import xyz.itwill09.dto.Userinfo;
+import xyz.itwill09.exception.BadRequestException;
+import xyz.itwill09.exception.ExistsUserinfoException;
 import xyz.itwill09.service.UserinfoService;
 
 @Controller
@@ -13,8 +21,100 @@ import xyz.itwill09.service.UserinfoService;
 public class UserinfoController {
 	private final UserinfoService userinfoService;
 	
+	//회원정보를 입력받기 위한 뷰이름을 반환하는 요청 처리 메소드
+	// => 비로그인 사용자 또는 관리자가 아닌 일반회원이 페이지를 요청할 경우 인위적 예외 발생 - 500 에러코드 발생
+	// => try~catch 구문을 사용하여 예외가 발생될 경우 에러메세지를 출력하는 뷰이름 반환 - 500 에러코드 미발생
+	/*
 	@RequestMapping(value = "/write", method = RequestMethod.GET)
-	public String write() {
+	public String write(HttpSession session) {
+		Userinfo loginUserinfo=(Userinfo)session.getAttribute("loginUserinfo");
+		try {
+			//페이지를 요청한 사용자가 비로그인 사용자이거나 관리자가 아닌 일반회원인 경우
+			if(loginUserinfo ==null || loginUserinfo.getStatus() != 9) {
+				throw new BadRequestException("비정상적인 방식으로 페이지를 요청 하였습니다.");
+			}
+		} catch (BadRequestException e) {
+			return "userinfo/user_error";
+		}
+		return "userinfo/user_write";	
+	}
+	*/
+	
+	//예외 처리 메소드(Exception Handle Method)를 사용하여 예외 처리
+	@RequestMapping(value = "/write", method = RequestMethod.GET)
+	public String write(HttpSession session) {
+		Userinfo loginUserinfo=(Userinfo)session.getAttribute("loginUserinfo");
+		//페이지를 요청한 사용자가 비로그인 사용자이거나 관리자가 아닌 일반회원인 경우
+		if(loginUserinfo ==null || loginUserinfo.getStatus() != 9) {
+			throw new BadRequestException("비정상적인 방식으로 페이지를 요청 하였습니다.");
+		}
+		return "userinfo/user_write";	
+	}
+	
+	/*
+	//회원정보를 전달받아 USERINFO 테이블에 삽입하고 로그인 페이지를 요청할 수 있는 URL 주소로
+	//응답하는 요청 처리 메소드
+	@RequestMapping(value = "/write", method = RequestMethod.POST)
+	public String write(@ModelAttribute Userinfo userinfo, Model model) {
+		try {
+			//매개변수로 전달받은 회원정보의 아이디가 중복될 경우 ExistsUserinfoException 발생
+			userinfoService.addUserinfo(userinfo);
+		} catch (ExistsUserinfoException e) {
+			model.addAttribute("message", e.getMessage());
+			model.addAttribute("userinfo", e.getUserinfo());
+			return "userinfo/user_write";	
+		}
+		return "redirect:/userinfo/login";	
+	}
+	*/
+	
+	//예외 처리 메소드(Exception Handle Method)를 사용하여 예외 처리
+	@RequestMapping(value = "/write", method = RequestMethod.POST)
+	public String write(@ModelAttribute Userinfo userinfo, Model model) {
+		userinfoService.addUserinfo(userinfo);
+		return "redirect:/userinfo/login";	
+	}
+	
+	//인증정보를 입력받기 위한 뷰이름을 반환하는 요청 처리 메소드
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login() {
+		return "userinfo/user_login";	
+	}
+	
+	//인증정보를 전달받아 USERINFO 테이블에 저장된 행을 검색해 인증 처리하고 환영메세지를 
+	//출력하는 뷰이름을 반환하는 요청 처리 메소드
+	// => 인증 성공 후 세션에 권한 관련 정보(회원정보)를 속성값으로 저장
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String login(@ModelAttribute Userinfo userinfo, HttpSession session) {
+		Userinfo authUserinfo=userinfoService.loginAuth(userinfo);
+		session.setAttribute("loginUserinfo", authUserinfo);	
+		return "userinfo/user_login";	
+	}
+	
+	//로그아웃 처리하고 로그인 페이지를 요청할 수 있는 URL 주소로 응답하는 요청 처리 메소드
+	@RequestMapping("/logout")
+	public String logout(HttpSession session) {
+		//session.removeAttribute("loginUserinfo");
+		session.invalidate();
+		return "redirect:/userinfo/login";	
+	}
+	
+	//@ExceptionHandler : 예외 처리 기능의 메소드를 설정하기 위한 어노테이션
+	// => Controller 클래스의 요청 처리 메소드에서 예외가 발생되면 Front Controller에 의해
+	//자동 호출되어 예외 처리하는 메소드 - 예외 처리 메소드
+	// => 예외 처리 메소드의 매개변수에 예외 처리에 필요한 객체를 전달받아 예외를 처리할 수
+	//있으며 클라이언트에게 응답할 뷰이름 반환 - 리다이렉트 이동가능
+	//value 속성 : 예외 처리하기 위한 클래스(Class 객체)를 속성값으로 설정
+	// => value 속성외에 다른 속성이 없는 경우 속성값만 설정 가능
+	@ExceptionHandler(value = BadRequestException.class)
+	public String badRequestExceptionHandler() {
+		return "userinfo/user_error";
+	}
+	
+	@ExceptionHandler(ExistsUserinfoException.class)
+	public String existsUserinfoExceptionHandler(ExistsUserinfoException exception, Model model) {
+		model.addAttribute("message", exception.getMessage());
+		model.addAttribute("userinfo", exception.getUserinfo());
 		return "userinfo/user_write";	
 	}
 }
---
+++ spring/src/main/webapp/WEB-INF/views/userinfo/user_login.jsp
@@ -0,0 +1,113 @@
+﻿<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>    
+<%-- 비로그인 상태의 사용자인 경우 - 사용자로부터 인증정보를 입력받기 위한 JSP 문서 --%>
+<%-- => [로그인] 태그를 클릭한 경우 [/userinfo/login] 페이지 요청 - 입력값(인증정보) 전달 --%>
+<%-- 로그인 상태의 사용자인 경우 - 환영메세지를 클라이언트에게 전달하여 응답하는 JSP 문서 --%>
+<%-- => [회원목록] 태그를 클릭한 경우 [/userinfo/list] 페이지 요청 --%>
+<%-- => [로그아웃] 태그를 클릭한 경우 [/userinfo/logout] 페이지 요청 --%>
+<%-- => [회원등록] 태그를 클릭한 경우 [/userinfo/write] 페이지 요청 - 관리자에게만 태그 제공 --%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>SPRING</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link rel=stylesheet href="<c:url value="/css/user.css"/>" type="text/css">
+<script language="JavaScript">
+function userLogin() {
+	if ( f.userid.value == "" ) {
+		alert("아이디를 입력하십시요.");
+		f.userid.focus();
+		return;
+	} 
+	if ( f.password.value == "" ) {
+		alert("비밀번호를 입력하십시요.");
+		f.password.focus();
+		return;
+	}	
+	
+	f.action = "<c:url value="/userinfo/login"/>";
+	f.submit();
+}
+</script>
+</head>
+<body bgcolor=#FFFFFF text=#000000 leftmargin=0 topmargin=0 marginwidth=0 marginheight=0>
+<br>
+<table width=780 border=0 cellpadding=0 cellspacing=0>
+	<tr>
+	  <td width="20"></td>
+	  <td style="color: red;">${message }</td>			
+	</tr>
+
+	<tr>
+	  <td width="20"></td>
+	  <td>
+  	  <!--contents-->
+	  <table width=590 border=0 cellpadding=0 cellspacing=0>
+		  <tr>
+			<td bgcolor="f4f4f4" height="22">&nbsp;&nbsp;<b>회원관리 - 로그인</b></td>
+		  </tr>
+	  </table>  
+	  <br>
+	  
+	  
+	  <c:choose>  
+		<%-- //비로그인 상태의 사용자인 경우 --%>
+		<c:when test="${empty(loginUserinfo) }">   	
+		  <!-- login Form  --> 
+		  <form name="f" method="post">
+		  <table border="0" cellpadding="0" cellspacing="1" width="590" bgcolor="BBBBBB">
+			  <tr>
+				<td width=100 align=center bgcolor="E6ECDE" height="22">사용자 아이디</td>
+				<td width=490 bgcolor="ffffff" style="padding-left:10px;">
+					<input type="text" style="width:150" name="userid" value="${userid }">
+				</td>
+			  </tr>
+			  <tr>
+				<td width=100 align=center bgcolor="E6ECDE" height="22">비밀번호</td>
+				<td width=490 bgcolor="ffffff" style="padding-left:10px;">
+					<input type="password" style="width:150" name="password">
+				</td>
+			  </tr>
+		  </table>
+		  </form>
+		  <br>
+		  
+		  <table width=590 border=0 cellpadding=0 cellspacing=0>
+			  <tr>
+				<td align=center>
+					<input type="button" value="로그인" onClick="userLogin();"> &nbsp;
+				</td>
+			  </tr>
+		  </table>
+	  	</c:when>
+	  	<c:otherwise><%-- 로그인 상태의 사용자인 경우 --%>
+
+		  <table border="0" cellpadding="0" cellspacing="1" width="590" bgcolor="BBBBBB">
+			  <tr>
+				<td align=center bgcolor="E6ECDE" height="22">
+					${loginUserinfo.name}님, 환영합니다.
+				</td>
+			  </tr>
+		  </table>
+		  <br>
+		  
+		  <table width=590 border=0 cellpadding=0 cellspacing=0>
+			  <tr>
+				<td align=center>
+					<button type="button" onclick="location.href='<c:url value="/userinfo/list"/>';">회원목록</button>
+					<button type="button" onclick="location.href='<c:url value="/userinfo/logout"/>';">로그아웃</button>
+					<c:if test="${loginUserinfo.status == 9 }">
+						<button type="button" onclick="location.href='<c:url value="/userinfo/write"/>';">회원등록</button>
+					</c:if>
+				</td>
+			  </tr>
+		  </table>
+		</c:otherwise>
+	  </c:choose>
+	  </td>
+	</tr>
+</table>  
+
+</body>
+</html>
