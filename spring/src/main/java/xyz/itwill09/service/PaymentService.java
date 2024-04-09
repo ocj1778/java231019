eb89f92a832bcc7338222efb411900ba2f197cd9
itwill <itwill@DESKTOP-AFNN3T8>
2024-04-09 13:20:22 +0900

    118Day

--- spring/src/main/java/xyz/itwill09/service/PaymentService.java
+++ spring/src/main/java/xyz/itwill09/service/PaymentService.java
@@ -3,6 +3,7 @@ package xyz.itwill09.service;
 import xyz.itwill09.dto.Payment;
 
 public interface PaymentService {
+	void addPayment(Payment payment);	
 	String getAccessToken(Payment payment);
 	Payment getPayment(String accessToken, String impUid);
 	String canclePayment(String accessToken, Payment payment);
--- spring/src/main/java/xyz/itwill09/service/PaymentServiceimpl.java
+++ spring/src/main/java/xyz/itwill09/service/PaymentServiceimpl.java
@@ -6,13 +6,19 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.stereotype.Service;
 
 import xyz.itwill09.dto.Payment;
 
 @Service
 public class PaymentServiceimpl implements PaymentService {
-
+	@Override
+	public void addPayment(Payment payment) {
+		System.out.println("결재정보를 테이블의 행으로 삽입하는 명령 실행 - DAO 클래스의 메소드 호출");
+	}
+	
 	//OpneAPI를 사용하기 위해 필요한 토큰을 발급받아 반환하는 메소드
 	@Override
 	public String getAccessToken(Payment payment) {
@@ -57,25 +63,140 @@ public class PaymentServiceimpl implements PaymentService {
 				br.close();
 				
 				System.out.println("result(Token) = "+result);//JSON 형식의 문자열
+				/*
+				{
+					"code": 0,
+					"message": null,
+					"response": {
+						"access_token": "a2e771c3432a0b2c7816d78732715e83d512d31c",
+					    "now": 1712634724,
+					    "expired_at": 1712636524
+				  	}
+				}
+				*/	
 				
 				//JSON 형식의 문자열을 Java 객체로 변환하기 위한 기능을 제공하는 json-simple 
 				//라이브러리를 프로젝트에 빌드 처리 - 메이븐 : pom.xml
-				
-				
+				//JSONParser 객체 : JSON 형식의 문자열을 Java 객체로 변환하기 위한 기능을 제공하는 객체
+				JSONParser jsonParser=new JSONParser();
+				//JSONParser.parse(String json) : 매개변수로 JSON 형식의 문자열을 전달받아
+				//Java 객체(Object 객체)로 반환하는 메소드
+				// => JSONObject 객체(Javascript의 Object 객체와 유사)로 형변환하여 저장
+				JSONObject jsonObject=(JSONObject)jsonParser.parse(result);
+				//JSONObject.get(String name) : JSONObject 객체에서 매개변수로 전달받은 속성명의
+				//속성값(Object 객체)을 반환하는 메소드
+				JSONObject responseObject=(JSONObject)jsonObject.get("response");
+				accessToken=(String)responseObject.get("access_token");
 			}
 		} catch (Exception e) {
-			// TODO: handle exception
+			e.printStackTrace();
 		}
-		
-		
-		return null;
+		return accessToken;
 	}
 
 	//하나의 결재정보를 제공하는 OpenAPI를 사용해 결재정보를 반환하는 메소드
 	@Override
 	public Payment getPayment(String accessToken, String impUid) {
-		// TODO Auto-generated method stub
-		return null;
+		Payment payment=new Payment();//응답결과를 저장하기 위해 Payment 객체 생성
+		//결재번호를 전달받아 결재정보를 제공받기 위한 OpenAPI의 URL 주소 저장
+		String apiURL="https://api.iamport.kr/payments/"+impUid;
+		try {
+			URL url=new URL(apiURL);
+			HttpURLConnection connection=(HttpURLConnection)url.openConnection();
+			connection.setRequestMethod("get");
+			connection.setRequestProperty("Authorization", accessToken);
+			int responseCode=connection.getResponseCode();
+			if(responseCode == 200) {
+				BufferedReader br=new BufferedReader(new InputStreamReader(connection.getInputStream()));
+				String input="";
+				String result="";
+				while((input = br.readLine()) != null) {
+					result+=input;
+				}
+				br.close();
+				
+				System.out.println("result(payment) = "+result);
+				/*
+				{
+					  "code": 0,
+					  "message": null,
+					  "response": {
+					    "amount": 10,
+					    "apply_num": "",
+					    "bank_code": null,
+					    "bank_name": null,
+					    "buyer_addr": "서울시 강남구 역삼동 내빌딩 5층 501호",
+					    "buyer_email": "ocj1778@hanmail.net",
+					    "buyer_name": "홍길동",
+					    "buyer_postcode": "123-456",
+					    "buyer_tel": "010-1234-5678",
+					    "cancel_amount": 10,
+					    "cancel_history": [
+					      {
+					        "pg_tid": "T507bb7d3aef3ee760ed",
+					        "amount": 10,
+					        "cancelled_at": 1695005582,
+					        "reason": "취소요청api",
+					        "receipt_url": "https://mockup-pg-web.kakao.com/v1/confirmation/p/T507bb7d3aef3ee760ed/b4b64a1eac2708bb6863f863bf4650967394f3d49647aa52f3ff03a4ce2c0a81",
+					        "cancellation_id": "06JC8FVHJTW2WY16FEY0"
+					      }
+					    ],
+					    "cancel_reason": "취소요청api",
+					    "cancel_receipt_urls": [
+					      "https://mockup-pg-web.kakao.com/v1/confirmation/p/T507bb7d3aef3ee760ed/b4b64a1eac2708bb6863f863bf4650967394f3d49647aa52f3ff03a4ce2c0a81"
+					    ],
+					    "cancelled_at": 1695005582,
+					    "card_code": null,
+					    "card_name": null,
+					    "card_number": null,
+					    "card_quota": 0,
+					    "card_type": null,
+					    "cash_receipt_issued": false,
+					    "channel": "pc",
+					    "currency": "KRW",
+					    "custom_data": null,
+					    "customer_uid": null,
+					    "customer_uid_usage": null,
+					    "emb_pg_provider": null,
+					    "escrow": false,
+					    "fail_reason": null,
+					    "failed_at": 0,
+					    "imp_uid": "imp_146565009559",
+					    "merchant_uid": "merchant_1695005564459",
+					    "name": "컴퓨터",
+					    "paid_at": 1695005582,
+					    "pay_method": "point",
+					    "pg_id": "TC0ONETIME",
+					    "pg_provider": "kakaopay",
+					    "pg_tid": "T507bb7d3aef3ee760ed",
+					    "receipt_url": "https://mockup-pg-web.kakao.com/v1/confirmation/p/T507bb7d3aef3ee760ed/b4b64a1eac2708bb6863f863bf4650967394f3d49647aa52f3ff03a4ce2c0a81",
+					    "started_at": 1695005565,
+					    "status": "cancelled",
+					    "user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
+					    "vbank_code": null,
+					    "vbank_date": 0,
+					    "vbank_holder": null,
+					    "vbank_issued_at": 0,
+					    "vbank_name": null,
+					    "vbank_num": null
+					  }
+				}
+				*/
+				JSONParser jsonParser=new JSONParser();
+				JSONObject jsonObject=(JSONObject)jsonParser.parse(result);
+				JSONObject responseObject=(JSONObject)jsonObject.get("response");
+				
+				payment.setImpUid((String)responseObject.get("imp_uid"));
+				payment.setMercharntUid((String)responseObject.get("merchant_uid"));
+				payment.setAmount((Long)responseObject.get("amount"));
+				payment.setStatus((String)responseObject.get("status"));
+			} else {
+				return null;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return payment;
 	}
 
 	//결재정보를 취소하는 OpenAPI를 사용해 취소결과를 반환하는 메소드
